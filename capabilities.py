46e6f41e61aded3902f13d796521b0d0b5d55990
Richard Gerkin <rgerkin@asu.edu>
2013-03-05 22:17:19 -0700

    ProducesSpikes, ReceivesCurrent, etc.

--- capabilities.py
+++ capabilities.py
@@ -1,14 +1,19 @@
 """Sciunit capability classes for neuroscience"""
 
-import sciunit.Capability
+from sciunit import Capability
 
-class ProducesMembranePotential(sciunit.Capability):
-	"""An array of membrane potential samples"""
+class ProducesMembranePotential(Capability):
+	"""An array of somatic membrane potential samples"""
 	def get_membrane_potential(self):
 		raise NotImplementedError()
 
-class ProducesSpikes(sciunit.Capability):
+class ProducesSpikes(Capability):
 	"""A 2-D array: spike_waveform x num_spikes""" 
 	def get_spikes(self):
+		raise NotImplementedError()
+
+class ReceivesCurrent(Capability):
+	"""An array of somatic injected current samples"""
+	def set_current(self,current):
 		raise NotImplementedError()
 
