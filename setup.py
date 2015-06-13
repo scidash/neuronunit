b47368f72f07e21157f70e2506c1f696f75a4283
Richard C Gerkin <rgerkin@asu.edu>
2015-06-13 12:30:10 -0700

    Added nml package to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version='0.1.2',
 	author='Rick Gerkin',
 	author_email='rgerkin@asu.edu',
-	packages=['neuronunit'],
+	packages=['neuronunit','neuronunit.nml'],
 	url='http://github.com/scidash/neuronunit',
 	license='MIT',
 	description='A SciUnit library for data-driven testing of single-neuron physiology models.',
