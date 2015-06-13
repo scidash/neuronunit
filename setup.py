18515fd395e268b517e52eb9ef8f2fca7e2bc5d6
Richard Gerkin <rgerkin@asu.edu>
2015-06-12 17:20:51 -0700

    Added setup.py and restructured package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='neuronunit',
+	version='0.1.2',
+	author='Rick Gerkin',
+	author_email='rgerkin@asu.edu',
+	packages=['neuronunit'],
+	url='http://github.com/scidash/neuronunit',
+	license='MIT',
+	description='A SciUnit library for data-driven testing of single-neuron physiology models.',
+	long_description="",
+	install_requires=[sciunit,numpy,scipy]
+)
