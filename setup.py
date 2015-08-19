48c1f1e42fb869bac601ad3dd717e705929eb6da
Richard C Gerkin <rgerkin@asu.edu>
2015-08-18 21:56:27 -0700

    Added elephant requirement

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
 	license='MIT',
 	description='A SciUnit library for data-driven testing of single-neuron physiology models.',
 	long_description="",
-	install_requires=['sciunit>=0.1.3.1','numpy','scipy','neo']
+	install_requires=['sciunit>=0.1.3.1','numpy','scipy','neo','elephant']
 )
