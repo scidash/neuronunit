999705c69ab49d45d76b199ff8661202117ab2cd
Richard C Gerkin <rgerkin@asu.edu>
2016-04-04 17:18:44 -0700

    Updated travis and versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
 	name='neuronunit',
-	version='0.1.8',
+	version='0.1.8.1',
 	author='Rick Gerkin',
 	author_email='rgerkin@asu.edu',
         packages=[
@@ -18,7 +18,7 @@ setup(
 	license='MIT',
 	description='A SciUnit library for data-driven testing of single-neuron physiology models.',
 	long_description="",
-	install_requires=['neo','elephant','sciunit>=0.1.5',],
+	install_requires=['neo','elephant','sciunit>=0.1.5.1',],
     dependency_links = ['git+http://github.com/neuralensemble/python-neo.git#egg=neo-0.4.0dev',
                         'https://github.com/scidash/sciunit/tarball/dev']
 )
