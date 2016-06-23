f2fef6a081d4f515a10072fc4fcf76bd8204ea50
Richard C Gerkin <rgerkin@asu.edu>
2016-06-23 15:13:51 -0700

    Updated requirements

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     license='MIT',
     description='A SciUnit library for data-driven testing of single-neuron physiology models.',
     long_description="",
-    install_requires=['neo','elephant','sciunit>=0.1.5.1',],
+    install_requires=['neo','elephant','sciunit>=0.1.5.2',],
     dependency_links = ['git+http://github.com/neuralensemble/python-neo.git#egg=neo-0.4.0dev',
                         'https://github.com/scidash/sciunit/tarball/dev']
 )
