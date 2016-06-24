2ff9f64fdf26e7bbead4cc13ff865832189ccc02
Richard C Gerkin <rgerkin@asu.edu>
2016-06-23 19:47:23 -0700

    Added scaling to the IVCurveTest

--- neuronunit/tests/channel.py
+++ neuronunit/tests/channel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy.optimize import minimize
 import quantities as pq
 
 import sciunit
@@ -11,16 +12,19 @@ class IVCurveTest(sciunit.Test):
     """Test the agreement between channel IV curves produced by models and those 
     observed in experiments"""
     
-    def __init__(self, observation, name='IV Curve Test', **params):
+    def __init__(self, observation, name='IV Curve Test', scale=False, 
+                 **params):
         self.validate_observation(observation)
         for key,value in observation.items():
             setattr(self, key, value)
+        self.scale = scale # Whether to scale the predicted IV curve to 
+                           # minimize distance from the observed IV curve
         self.converter = AtMostToBoolean(pq.Quantity(1.0,'pA**2'))
         super(IVCurveTest,self).__init__(observation, name=name, **params)
     
     required_capabilities = (ProducesIVCurve,)
     score_type = BooleanScore
-    meta = True # base class for deriving useful test classes.  
+    meta = True # base class for deriving useful test classes 
 
     def validate_observation(self, observation):
         assert type(observation) is dict
@@ -29,9 +33,9 @@ class IVCurveTest(sciunit.Test):
             assert type(observation[item]) in [list,tuple] \
                 or isinstance(observation[item],np.ndarray)
         if hasattr(observation['v'],'units'):
-            observation['v'] = observation['v'].rescale(pq.V) # Rescale to V.  
+            observation['v'] = observation['v'].rescale(pq.V) # Rescale to V 
         if hasattr(observation['i'],'units'):
-            observation['i'] = observation['i'].rescale(pq.pA) # Rescale to pA.      
+            observation['i'] = observation['i'].rescale(pq.pA) # Rescale to pA     
     
     def generate_prediction(self, model, verbose=False):
         raise Exception(("This is a meta-class for tests; use tests derived "
@@ -41,13 +45,13 @@ class IVCurveTest(sciunit.Test):
         """
         Interpolate IV curve along a larger and evenly spaced range of holding 
         potentials. Ensures that test is restricted to ranges present in both 
-        predicted and observed data.  
+        predicted and observed data.
         v_pred: The array of holding potentials in model.  
         i_pred: The array or dict of resulting currents (single values) 
-                from model.  
+                from model .
         v_obs: The array of holding potentials from the experiments.  
         i_obs: The array or dict of resulting currents (single values) 
-               from the experiment.  
+               from the experiment .
         """
         
         if type(i_obs)==dict:
@@ -72,8 +76,8 @@ class IVCurveTest(sciunit.Test):
         stop = min(v_obs[-1],v_pred[-1]) # Max voltage in mV
         
         v_new = np.linspace(start, stop, 100) # 1 mV interpolation
-        i_obs_interp = f_obs(v_new)*pq.pA # Interpolated current from data.  
-        i_pred_interp = f_pred(v_new)*pq.pA # Interpolated current from model.  
+        i_obs_interp = f_obs(v_new)*pq.pA # Interpolated current from data  
+        i_pred_interp = f_pred(v_new)*pq.pA # Interpolated current from model
         
         return {'v':v_new*pq.mV, 'i_pred':i_pred_interp, 'i_obs':i_obs_interp}
     
@@ -82,8 +86,21 @@ class IVCurveTest(sciunit.Test):
         o = observation
         p = prediction
         interped = self.interp_IV_curves(o['v'], o['i'], p['v'], p['i'])
-        self.interped = interped
+        
+        if self.scale:
+            def f(sf):
+                score = FloatScore.compute_ssd(interped['i_obs'],
+                                               (10**sf)*interped['i_pred'])
+                return score.score.magnitude
+            result = minimize(f,0.0)
+            scale_factor = 10**result.x
+            interped['i_pred'] *= scale_factor
+        else:
+            scale_factor = 1
+        
         score = FloatScore.compute_ssd(interped['i_obs'],interped['i_pred'])
+        score.related_data['scale_factor'] = scale_factor
+        self.interped = interped
         return score
 
     def bind_score(self,score,model,observation,prediction):
@@ -93,7 +110,7 @@ class IVCurveTest(sciunit.Test):
         # Observation and prediction are automatically stored in score, 
         # but since that is before interpolation, we store the interpolated 
         # versions as well.  
-        score.related_data = self.interped
+        score.related_data.update(self.interped)
         score.plot = self.last_model.plot_iv_curve
         return score
 
