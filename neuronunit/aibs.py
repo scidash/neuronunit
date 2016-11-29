6d225a06aac69bb7faaa1c6e6f89779b005f2a0b
Russell Jarvis <rjjarvis@asu.edu>
2016-11-28 18:42:35 -0700

    improved fix explanation moderately

--- neuronunit/aibs.py
+++ neuronunit/aibs.py
@@ -45,12 +45,13 @@ def get_sp(experiment_params,sweep_ids):
     if sweep_num is None:
         raise Exception('Sweep with ID %d not found in dataset with ID %d.' % (sweep_id, dataset_id))
     return sp
-   
+
+       
 def get_value_dict(experiment_params,sweep_ids,kind,cached=True):
     '''
-    A candidate method for replacing get_sweep_params observation.
+    A candidate method for replacing get_observation.
     This fix is necessary due to changes in the allensdk however:
-    Warning. This method may not properly convey the meaning of get_sweep_params
+    Warning. Togethor with get_sp this method may not properly convey the meaning of get_observation
     '''
     ct = CellTypesApi()
     if kind == str('rheobase'):
