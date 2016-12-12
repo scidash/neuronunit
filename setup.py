ff253e1712c26a709f052a21fcbcfe464d56e1ec
Richard C Gerkin <rick1@gerkin.org>
2016-12-11 23:21:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
     description='A SciUnit library for data-driven testing of single-neuron physiology models.',
     long_description="",
     install_requires=['scipy>=0.17','matplotlib>=1.5','neo==0.4','elephant','sciunit==0.1.5.6','allensdk==0.12.4.1'],
-    dependency_links = ['https://github.com/scidash/sciunit/tarball/dev#egg=sciunit-0.1.5.5',
+    dependency_links = ['https://github.com/scidash/sciunit/tarball/dev#egg=sciunit-0.1.5.6',
                         'https://github.com/rgerkin/AllenSDK/tarball/python3.5#egg=allensdk-0.12.4.1']
 )
