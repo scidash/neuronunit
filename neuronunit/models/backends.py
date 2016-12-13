5bd1625bbca251e14f27447d877f490b506f1712
Russell Jarvis <colouredstatic@gmail.com>
2016-12-13 12:04:14 -0700

    fleshed out code stubs for NEURONbackend
    Naming of methods is a bit different to the names requested, but thats just because its a bit ambiguous about what is intended.
    
    Ie loading the model is really initializing the model, which could arguably occur in the objects constructor method.
    
    `set_attrs` is ambiguous with `update_run_params` in the sense that often simulation params/model params are also simulation attributes/model attributes.
    
    It was also requested that current injection parameters (or stimulation protocol parameters) be modularized out from the setting of cell model parameters, as is done here, but the name inject_current was not used, only because I am unsure of the method signature preferred, specifically I am unsure about what arguments it needs.

--- neuronunit/models/backends.py
+++ neuronunit/models/backends.py
@@ -28,8 +28,129 @@ class jNeuroMLBackend(Backend):
     def update_run_params(self, attrs):
         self.update_lems_run_params(attrs)
 
-        
 
+
+class NEURONBackend(Backend):
+    def __init__(self,nml_file_path):#If neuron doesn't exist yet, then create it, however really this is just a place holder for 
+        self.neuron=None#Its only important that neuron is an object attribute, such that 
+        #it has non local scope in the methods below. neuron acts a bit like a global variable object type whose contents are constantly mutated.
+        self.nml_file=nml_file_path
+    
+    def load_model(self):        
+        '''
+        Take a declarative model description, and convert it into an implementation, stored in a pyhoc file.
+        import the pyhoc file thus dragging the neuron variables into memory/python name space.
+        Since this only happens once outside of the optimization loop its a tolerable performance hit.
+        '''
+        #could this be a shorter root to NeuroML2/LEMS_2007One.xml saving the user from needing to supply it given NeuroML2 may already be installed?
+        #izc=nml.nml.Izhikevich2007Cell
+  
+        DEFAULTS={}
+        DEFAULTS['v']=True
+        #Create a pyhoc file using jneuroml to convert from NeuroML to pyhoc.
+        #TODO
+        #also check if cached file NeuroML2/LEMS_2007One_nrn.py exists before exec: 
+        pynml.run_lems_with_jneuroml_neuron(self.nml_file, 
+                                          skip_run=False,
+                                          nogui=False, 
+                                          load_saved_data=False, 
+                                          plot=False, 
+                                          show_plot_already=False, 
+                                          exec_in_dir = ".",
+                                          only_generate_scripts = True,
+                                          verbose=DEFAULTS['v'],
+                                          exit_on_fail = True)
+        
+        #import the contents of the file into the current names space.
+        from NeuroML2 import LEMS_2007One_nrn 
+        self.neuron=LEMS_2007One_nrn.neuron
+        #make sure mechanisms are loaded
+        self.neuron.load_mechanisms(os.getcwd()+'/NeuroML2')    
+        #import the default simulation protocol
+        from NeuroML2.LEMS_2007One_nrn import NeuronSimulation
+        #self.although this may be unnecessary.
+        self.ns = NeuronSimulation(tstop=1600, dt=0.0025)
+
+    def update_nrn_param(self,param_dict):
+        #TODO find out the python3 syntax for dereferencing key value pairs.
+        #Below syntax is stupid, but how to just get key generically without for knowledge of its name and without iterating?
+        items=[ (key, value) for key,value in param_dict.items() ]
+        for key, value in items:
+           print(key, value) 
+           #TODO make this solution such that it would work for other single cell models specified by a converted  from neuroml to pyhoc file.
+           evalstring='self.neuron.hoc.execute("m_RS_RS_pop[0].'+str(key)+'='+str(value)+'")'
+           eval(evalstring)
+        #neuron.hoc.execute('forall{ psection() }')
+
+    def update_inject_current(self,stim_dict):
+        #TODO find out the python3 syntax for dereferencing key value pairs.
+        #Below syntax is stupid, but how to just get key generically without for knowledge of its name and without iterating?
+        items=[ (key, value) for key,value in stim_dict.items() ]
+        for key, value in items:
+           print(key, value)
+           #TODO make this solution such that it would work for other single cell models specified by a converted from neuroml to pyhoc file.
+           evalstring='self.neuron.hoc.execute("explicitInput_RS_IextRS_pop0.'+str(key)+'='+str(value)+'")'
+           print(evalstring) 
+           eval(evalstring)
+        self.neuron.hoc.execute('forall{ psection() }')
+        
+    def local_run(self,duration=1600):
+        '''
+        Optional argument time duration. 
+        Executes the neuron simulation specified in the context of the context of NEURONbackend class
+        '''
+        initialized = True
+        sim_start = time.time()
+        self.neuron.hoc.execute('tstop='+str(duration))
+        self.neuron.hoc.execute('dt=0.0025')
+        self.neuron.hoc.tstop=1600
+        self.neuron.hoc.dt=0.0025   
+        
+        print("Running a simulation of %sms (dt = %sms)" % (self.neuron.hoc.tstop, self.neuron.hoc.dt))
+        #pdb.set_trace()
+        self.neuron.hoc.execute('run()')
+        self.neuron.hoc.execute('forall{ psection() }')
+
+        sim_end = time.time()
+        sim_time = sim_end - sim_start
+        print("Finished NEURON simulation in %f seconds (%f mins)..."%(sim_time, sim_time/60.0))
+    
+        
+    def out_to_neo(self):
+        '''
+        Extracts HOC recording variables from the HOC object namespace,
+        converts them to neo analog signal and returns a tuple. Each element in the tuple is of neo
+        Analog signal type.
+        WARNING probably bug(s). Time conversion not done. Raw time just handed straight to neo without thinking about it.
+        implemented conversions from other sources without thinking about it!
+        conversion that I have applied here probably doesn't apply
+        '''
+        from neo.core import AnalogSignal
+        import quantities as pq
+        t = [ t/1000 for t in self.neuron.h.v_time.to_python() ]  # Convert to Python list for speed...
+        v = [ float(x  / 1000.0) for x in self.neuron.h.v_v_of0.to_python() ]  # Convert to Python list for speed, variable has dim: voltage
+        current = [ float(x  / 1.0E9) for x in self.neuron.h.v_u_of0.to_python() ]  # Convert to Python list for speed, variable has dim: current
+        dt = (t[1]-t[0])*pq.s # Time per sample in milliseconds.  
+        vm = AnalogSignal(v,units=pq.V,sampling_rate=1.0/dt)
+        im = AnalogSignal(current,units=pq.V,sampling_rate=1.0/dt)
+        time = AnalogSignal(t,units=pq.ms,sampling_rate=1.0/dt)
+        return (vm,im,time)
+    
+
+
+    def advance(self):
+        '''
+        This method might not do anything.
+        '''
+        #over write/load regular advance
+
+        if not initialized:
+            neuron.hoc.finitialize()
+            initialized = True
+
+        neuron.hoc.fadvance()
+        
+'''
 class NEURONBackend(Backend):
     f = pynml.run_lems_with_jneuroml_neuron
 
@@ -41,3 +162,4 @@ class NEURONBackend(Backend):
 
     def update_run_params(self):
         pass
+'''
