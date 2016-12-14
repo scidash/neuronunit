383ca4fee0b360453d7e0ce328b652b4ba19be3f
Russell Jarvis <colouredstatic@gmail.com>
2016-12-13 18:50:35 -0700

    a file thats much closer to satisfying neuronunit.capabilities
    As above.

--- neuronunit/models/backends.py
+++ neuronunit/models/backends.py
@@ -1,22 +1,12 @@
 from pyneuroml import pynml
-
-
-class Backend:
+import os
+import sciunit
+import time
+import pdb
+class Backend(sciunit.Model):
     # The function (e.g. from pynml) that handles running the simulation
     f = None
 
-    def load_model(self):
-        # Loads the model into memory
-        pass
-
-    def set_attrs(self, attrs):
-        # Set attributes (e.g. resting potential) of model in memory or on disk
-        pass
-
-    def update_run_params(self):
-        # Set attributes (e.g. somatic current injection) of simulation 
-        # in memory or on disk
-        pass
 
 
 class jNeuroMLBackend(Backend):
@@ -31,7 +21,7 @@ class jNeuroMLBackend(Backend):
 
 
 class NEURONBackend(Backend):
-    def __init__(self,nml_file_path):
+    def __init__(self,nml_file_path,name=None,attrs=None):
         '''
         Arguably nml_file_path can move out of the constructor signature, and into load_model signature.
         self.neuron is just a place holder for the neuron object attribute. 
@@ -40,8 +30,11 @@ class NEURONBackend(Backend):
         in the scope of this class whose contents are constantly mutated.
         '''
         self.neuron=None
-        self.nml_file=nml_file_path
-    
+        self.nml_file=nml_file_path+str('/LEMS_2007One.xml')
+        self.nml_file_path=nml_file_path
+        self.name=name
+        self.attrs=attrs
+
     def load_model(self):        
         '''
         Take a declarative model description, and convert it into an implementation, stored in a pyhoc file.
@@ -56,28 +49,36 @@ class NEURONBackend(Backend):
         #Create a pyhoc file using jneuroml to convert from NeuroML to pyhoc.
         #TODO
         #also check if cached file NeuroML2/LEMS_2007One_nrn.py exists before exec: 
-        pynml.run_lems_with_jneuroml_neuron(self.nml_file, 
+        f=pynml.run_lems_with_jneuroml_neuron(self.nml_file, 
                                           skip_run=False,
                                           nogui=False, 
                                           load_saved_data=False, 
-                                          plot=False, 
+                                           only_generate_scripts = True,
+                                         plot=False, 
                                           show_plot_already=False, 
                                           exec_in_dir = ".",
-                                          only_generate_scripts = True,
                                           verbose=DEFAULTS['v'],
                                           exit_on_fail = True)
         
         #import the contents of the file into the current names space.
-        from NeuroML2 import LEMS_2007One_nrn 
+        from neuronunit.tests.NeuroML2 import LEMS_2007One_nrn 
         self.neuron=LEMS_2007One_nrn.neuron
         #make sure mechanisms are loaded
-        self.neuron.load_mechanisms(os.getcwd()+'/NeuroML2')    
+        self.neuron.load_mechanisms(self.nml_file_path)    
         #import the default simulation protocol
-        from NeuroML2.LEMS_2007One_nrn import NeuronSimulation
+        from neuronunit.tests.NeuroML2.LEMS_2007One_nrn import NeuronSimulation
         #this next step may be unnecessary: TODO delete it and check.
         self.ns = NeuronSimulation(tstop=1600, dt=0.0025)
+        #return NeuronSimulation
+        return self
 
-    def update_nrn_param(self,param_dict):
+    #def set_attrs(self, attrs):
+        # Set attributes (e.g. resting potential) of model in memory or on disk
+    #    pass
+        
+    def set_attrs(self,attrs):
+        print(attrs)
+       
         #TODO find out the python3 syntax for dereferencing key value pairs.
         #Below syntax is stupid, but how to just get key generically without for knowledge of its name and without iterating?
         items=[ (key, value) for key,value in param_dict.items() ]
@@ -88,8 +89,15 @@ class NEURONBackend(Backend):
            eval(evalstring)
         #neuron.hoc.execute('forall{ psection() }')
 
-    def update_inject_current(self,stim_dict):
-        #TODO find out the python3 syntax for dereferencing key value pairs.
+    #def inject_current(self,stim_dict):
+        # Set attributes (e.g. somatic current injection) of simulation 
+        # in memory or on disk
+        #pass
+
+
+    def update_run_params(self,attrs):
+        print(attrs)
+       #TODO find out the python3 syntax for dereferencing key value pairs.
         #Below syntax is stupid, but how to just get key generically without for knowledge of its name and without iterating?
         items=[ (key, value) for key,value in stim_dict.items() ]
         for key, value in items:
@@ -99,12 +107,101 @@ class NEURONBackend(Backend):
            print(evalstring) 
            eval(evalstring)
         self.neuron.hoc.execute('forall{ psection() }')
+
+       
+    def get_membrane_potential(self):
+        '''
+        method required by neuronunit/sciunit
+        '''
+        '''
+        Extracts HOC recording variables from the HOC object namespace,
+        converts them to neo analog signal and returns a tuple. Each element in the tuple is of neo
+        Analog signal type.
+        WARNING probably bug(s). Time conversion not done. Raw time just handed straight to neo without thinking about it.
+        implemented conversions from other sources without thinking about it!
+        conversion that I have applied here probably doesn't apply
+        '''
+        from neo.core import AnalogSignal
+        import quantities as pq
+        #The pyhoc file generated by pyNeuroML does the conversions below:
+        #voltage is scaled by 1000 (to make it millivolts).
+        #current is scaled by 1,000,000,000 (to make it nanoAmps)
+        #LEMS_2007One_nrn.py
+        #t = [ t/1000 for t in self.neuron.h.v_time.to_python() ]  # Convert to Python list for speed...
+        #v = [ float(x  / 1000.0) for x in self.neuron.h.v_v_of0.to_python() ]  # Convert to Python list for speed, variable has dim: voltage
+        #current = [ float(x  / 1.0E9) for x in self.neuron.h.v_u_of0.to_python() ]  # Convert to Python list for speed, variable has dim: current
+        #probably this can be done in the invocation of AnalogSignal below instead.
+        t = self.neuron.h.v_time.to_python()   # Convert to Python list for speed...
+        v = self.neuron.h.v_v_of0.to_python()   # Convert to Python list for speed, variable has dim: voltage
+        current = [ self.neuron.h.v_u_of0.to_python() ]  # Con
+        dt = (t[1]-t[0])*pq.s # Time per sample in milliseconds.  
+        vm = AnalogSignal(v,units=pq.mV,sampling_rate=1.0/dt)
+        im = AnalogSignal(current,units=pq.nA,sampling_rate=1.0/dt)
+        time = AnalogSignal(t,units=pq.ms,sampling_rate=1.0/dt)
+        return vm#(vm,im,time)
+
+    
+    def get_spike_train(self):
+        from neuronunit.capabilities import spike_functions
+        st=spike_functions.get_spike_train(self.get_membrane_potential(),threshold=0.0*mV)
+        return spike_functions.get_spike_train(self.get_membrane_potential())
+    
+    def get_spike_count(self):
+  
+        spike_train = self.get_spike_train()
+        print(self.get_membrane_potential())
+        print(spike_train)
+        print('this is the spike count:')
+        print(len(spike_train))
+        return len(spike_train)
+   
+    
+        
+
+    def inject_square_current(self,current):
+        '''
+        warning ARE THE UNITS correct? 
+        DONOT merge until question is resolved.
+        method required by neuronunit/sciunit
+        '''
+        
+        '''                                                                                            
+        current : a dictionary like:                                                                                                                                          
+        {'amplitude':-10.0*pq.pA,                                                                                                                             
+         'delay':100*pq.ms,                                                                                                                                   
+         'duration':500*pq.ms}}                                                                                                                               
+        where 'pq' is the quantities package        
+        '''
+        import pq
+        import re
+
+        current['amplitude'] = re.sub('\ pA$', '', str(current['amplitude']))
+
+        amps=float(current['amplitude'])/1000.0
+        evalstring='self.neuron.hoc.execute("explicitInput_RS_IextRS_pop0.'+str('amplitude')+'='+str(amps)+'")'
+        print(evalstring) 
+        eval(evalstring)
+        #evalstring='self.neuron.hoc.execute("explicitInput_RS_IextRS_pop0.'+str('duration')+'='+str(1000.0)+'")'
+        #print(evalstring) 
+        eval(evalstring)
+
+        self.neuron.hoc.execute('forall{ psection() }')
+
+
+        #TODO find out the python3 syntax for dereferencing key value pairs.
+        #Below syntax is stupid, but how to just get key generically without for knowledge of its name and without iterating?
+        #items=[ (key, value) for key,value in stim_dict.items() ]
+        #for key, value in items:
+        #   print(key, value)
+           #TODO make this solution such that it would work for other single cell models specified by a converted from neuroml to pyhoc file.
+          
         
     def local_run(self,duration=1600):
         '''
         Optional argument time duration. 
         Executes the neuron simulation specified in the context of the context of NEURONbackend class
         '''
+        
         initialized = True
         sim_start = time.time()
         self.neuron.hoc.execute('tstop='+str(duration))
@@ -133,7 +230,7 @@ class NEURONBackend(Backend):
         '''
         from neo.core import AnalogSignal
         import quantities as pq
-        
+        import pdb
         #The pyhoc file generated by pyNeuroML does the conversions below:
         #voltage is scaled by 1000 (to make it millivolts).
         #current is scaled by 1,000,000,000 (to make it nanoAmps)
@@ -142,17 +239,16 @@ class NEURONBackend(Backend):
         #v = [ float(x  / 1000.0) for x in self.neuron.h.v_v_of0.to_python() ]  # Convert to Python list for speed, variable has dim: voltage
         #current = [ float(x  / 1.0E9) for x in self.neuron.h.v_u_of0.to_python() ]  # Convert to Python list for speed, variable has dim: current
         #probably this can be done in the invocation of AnalogSignal below instead.
-        t = [ self.neuron.h.v_time.to_python() ]  # Convert to Python list for speed...
-        v = [ self.neuron.h.v_v_of0.to_python() ]  # Convert to Python list for speed, variable has dim: voltage
+        t = self.neuron.h.v_time.to_python()   # Convert to Python list for speed...
+        v = self.neuron.h.v_v_of0.to_python()   # Convert to Python list for speed, variable has dim: voltage
         current = [ self.neuron.h.v_u_of0.to_python() ]  # Con
-        
+        #pdb.set_trace()
         dt = (t[1]-t[0])*pq.s # Time per sample in milliseconds.  
         vm = AnalogSignal(v,units=pq.mV,sampling_rate=1.0/dt)
         im = AnalogSignal(current,units=pq.nA,sampling_rate=1.0/dt)
         time = AnalogSignal(t,units=pq.ms,sampling_rate=1.0/dt)
         return (vm,im,time)
     
-
 
     def advance(self):
         '''
