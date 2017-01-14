783521abde61f84715173f6693a70c8fcd8e83ee
Russell Jarvis <rjjarvis@asu.edu>
2017-01-14 15:36:38 -0700

    some parellel stuff and cloud build

--- neuronunit/software_tests/Optimize.py
+++ neuronunit/software_tests/Optimize.py
@@ -33,26 +33,7 @@ LEMS_MODEL_PATH = IZHIKEVICH_PATH+str('/LEMS_2007One.xml')
 import time
 
 from pyneuroml import pynml
-'''
-recorded_times=[]
-before_pynml=time.time()
-f=pynml.run_lems_with_jneuroml_neuron
-DEFAULTS={}
-DEFAULTS['v']=True
-results=pynml.run_lems_with_jneuroml_neuron(LEMS_MODEL_PATH,
-                  skip_run=False,
-                  nogui=True,
-                  load_saved_data=True,
-                  only_generate_scripts = False,
-                  plot=False,
-                  show_plot_already=False,
-                  exec_in_dir = ".",
-                  verbose=DEFAULTS['v'],
-exit_on_fail = True)
-after_pynml=time.time()
-delta_pynml=after_pynml-before_pynml
-print('jneuroml time: ',after_pynml-before_pynml)
-'''
+
 import quantities as pq
 from neuronunit import tests as nu_tests, neuroelectro
 neuron = {'nlex_id': 'nifext_50'} # Layer V pyramidal cell
@@ -108,7 +89,6 @@ def update_amplitude(test,tests,score):
 #Do the rheobase test. This is a serial bottle neck that must occur before any parallel optomization.
 #Its because the optimization routine must have apriori knowledge of what suprathreshold current injection values are for each model.
 
-
 hooks = {tests[0]:{'f':update_amplitude}} #This is a trick to dynamically insert the method
 #update amplitude at the location in sciunit thats its passed to, without any loss of generality.
 suite = sciunit.TestSuite("vm_suite",tests,hooks=hooks)
@@ -118,31 +98,15 @@ from neuronunit.models.reduced import ReducedModel
 model = ReducedModel(LEMS_MODEL_PATH,name='vanilla',backend='NEURON')
 model=model.load_model()
 model.local_run()
-'''
-before_nrn=time.time()
 
-#Its because Reduced model is the base class that calling super on SingleCellModel does not work.
-model = ReducedModel(LEMS_MODEL_PATH,name='vanilla',backend='NEURON')
-model=model.load_model()
-model.local_run()
-after_nrn=time.time()
-times_nrn=after_nrn-before_nrn
-print(times_nrn)
-delta_pynml
-delta_nrn_pynml=np.abs(delta_pynml-times_nrn)
-print('the time difference is: \n')
-print(delta_nrn_pynml)
-print('press c to continue')
-pdb.set_trace()
-'''
 from types import MethodType
 def optimize(self,model,rov,param):
     best_params = None
     best_score = None
     from neuronunit.deapcontainer.deap_container2 import DeapContainer
     dc=DeapContainer()
-    pop_size=12
-    ngen=5
+    pop_size=8
+    ngen=0
 
     #commited the change: pass in the model to deap, don't recreate it in every iteration just mutate the one existing model.
     #arguably recreating it would lead to less bugs however so maybe change back later.
@@ -161,6 +125,7 @@ def optimize(self,model,rov,param):
 #toolbox = base.Toolbox()
 #from scoop import futures
 if __name__ == '__main__':
+
 #toolbox.register("map", futures.map)
     my_test = tests[0]
     my_test.verbose = True
@@ -196,44 +161,3 @@ if __name__ == '__main__':
         for i in xrange(0,9):
             plt.plot(pop[i].time_trace,pop[i].voltage_trace)
         plt.savefig('best 10')
-#NeuronObject=backends.NEURONBackend(LEMS_MODEL_PATH)
-#NeuronObject.load_model()#Only needs to occur once
-#NeuronObject.update_nrn_param(param_dict)
-#NeuronObject.update_inject_current(stim_dict)
-'''
-TODO: change such that it execs simulations.
-Note this is not actually running any simulations.
-Its just initialising them.
-brute force optimization:
-for comparison
-#print(dir(NeuronObject))
-for vr in iter(np.linspace(-75,-50,6)):
-    for i,a in iter(enumerate(np.linspace(0.015,0.045,7))):
-        for j,b in iter(enumerate(np.linspace(-3.5,-0.5,7))):
-            for k in iter(np.linspace(100,200,4)):
-                param_dict={}#Very important to redeclare dictionary or badness.
-                param_dict['vr']=vr
-
-                param_dict['a']=str(a)
-                param_dict['b']=str(b)
-                param_dict['C']=str(150)
-                param_dict['k']=str(0.70)
-                param_dict['vpeak']=str(45)
-
-                NeuronObject.update_nrn_param(param_dict)
-                stim_dict={}
-                stim_dict['delay']=200
-                stim_dict['duration']=500
-                stim_dict['amplitude']=k#*100+150
-
-                NeuronObject.update_inject_current(stim_dict)
-                NeuronObject.local_run()
-                vm,im,time=NeuronObject.out_to_neo()
-                print('\n')
-                print('\n')
-                print(vm.trace)
-                print(time.trace)
-                print(im.trace)
-                print('\n')
-                print('\n')
-'''
