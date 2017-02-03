7336bc60cc07ff0f11f94e270b209ee874a3b66d
Russell Jarvis <rjjarvis@asu.edu>
2017-02-02 18:04:56 -0700

    merge into regular dev

--- neuronunit/tests/NeuroML2/LEMS_2007One_nrn.py
+++ neuronunit/tests/NeuroML2/LEMS_2007One_nrn.py
@@ -134,7 +134,7 @@ class NeuronSimulation():
 
         # File to save: time
         #py_v_time = [ t/1000 for t in h.v_time.to_python() ]  # Convert to Python list for speed...
-        py_v_time = [ t for t in h.v_time.to_python() ]  # Convert to Python list for speed...
+        py_v_time = [ float(t) for t in h.v_time.to_python() ]  # Convert to Python list for speed...
 
         f_time_f2 = open('time.dat', 'w')
         num_points = len(py_v_time)  # Simulation may have been stopped before tstop...
@@ -145,8 +145,11 @@ class NeuronSimulation():
         print("Saved data to: time.dat")
 
         # File to save: of0
-        py_v_v_of0 = [ float(x  / 1000.0) for x in h.v_v_of0.to_python() ]  # Convert to Python list for speed, variable has dim: voltage
-        py_v_u_of0 = [ float(x  / 1.0E9) for x in h.v_u_of0.to_python() ]  # Convert to Python list for speed, variable has dim: current
+        py_v_v_of0 = [ float(x) for x in h.v_v_of0.to_python() ]  # Convert to Python list for speed, variable has dim: voltage
+        py_v_u_of0 = [ float(x) for x in h.v_u_of0.to_python() ]  # Convert to Python list for speed, variable has dim: current
+
+        #py_v_v_of0 = [ float(x  / 1000.0) for x in h.v_v_of0.to_python() ]  # Convert to Python list for speed, variable has dim: voltage
+        #py_v_u_of0 = [ float(x  / 1.0E9) for x in h.v_u_of0.to_python() ]  # Convert to Python list for speed, variable has dim: current
 
         f_of0_f2 = open('RS_One.dat', 'w')
         num_points = len(py_v_time)  # Simulation may have been stopped before tstop...
