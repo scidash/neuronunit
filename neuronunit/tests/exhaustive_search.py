4869ebcb81c938154ad5163d421b46a2ebe5fbe0
Russell Jarvis <rjjarvis@asu.edu>
2017-02-04 16:49:26 -0700

    added mpi4py back in

--- neuronunit/tests/exhaustive_search.py
+++ neuronunit/tests/exhaustive_search.py
@@ -16,24 +16,52 @@ model=model.load_model()
 vr = np.linspace(-75.0,-50.0,10)
 a = np.linspace(0.015,0.045,10)
 b = np.linspace(-3.5*10E-9,-0.5*10E-9,10)
-iter_list=[ (i,j,k) for i in a for j in b for k in vr ]
+k = np.linspace(7.0E-4-+7.0E-5,7.0E-4+70E-5,10)
+C = np.linspace(1.00000005E-4-1.00000005E-5,1.00000005E-4+1.00000005E-5,10)
+
+c = np.linspace(-55,-60,10)
+d = np.linspace(0.050,0.2,10)
+v0 = np.linspace(-75.0,-45.0,10)
+vt =  np.linspace(-50.0,-30.0,10)
+
+'''
+iter_list=[ (i,j,r,l,m,n,o,p,q) for i in a for j in b \
+                                 for r in vr for l in k \
+                                 for m in C for n in c \
+                                 for o in d for p in v0 for q in vt ]
+'''
+iter_list=[ (i,j,k) for i in a for j in b for k in vr for q in vt ]
 
 def func2map(iter_arg):
     attrs={}
     attrs['//izhikevich2007Cell']={}
-    param=['a','b','vr']
-    i,j,k=iter_list[iter_arg]
+    param=['a','b','vr','vt']
+    #param=['a','b','vr','k','C','c','d','v0','vt']
+    i,j,r,q=iter_list[iter_arg]
+
+    #i,j,r,l,m,n,o,p,q=iter_list[iter_arg]
     model.name=str(i)+str(j)+str(k)
     attrs['//izhikevich2007Cell']['a']=i
     attrs['//izhikevich2007Cell']['b']=j
-    attrs['//izhikevich2007Cell']['vr']=k
+    attrs['//izhikevich2007Cell']['vr']=r
+    attrs['//izhikevich2007Cell']['vt']=q
+
+    '''
+    attrs['//izhikevich2007Cell']['k']=l
+    attrs['//izhikevich2007Cell']['C']=m
+    attrs['//izhikevich2007Cell']['c']=n
+    attrs['//izhikevich2007Cell']['d']=o
+    attrs['//izhikevich2007Cell']['v0']=p
+    '''
+    print(attrs)
+
     model.update_run_params(attrs)
     score = get_neab.suite.judge(model)#passing in model, changes model
     model.run_number+=1
     RUN_TIMES='{}{}{}'.format('counting simulation run times on models',model.results['run_number'],model.run_number)
     return score
 
-iter_arg = [ i for i in range(RANK, 999, SIZE) ]
+iter_arg = [ i for i in range(RANK, len(iter_list), SIZE) ]
 
 bg_bf=time.time()
 score_matrix = [func2map(i) for i in iter_arg]
