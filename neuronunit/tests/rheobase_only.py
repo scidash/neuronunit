f7ddc472acb904c1f53487e44d600e91be803fae
Russell Jarvis <rjjarvis@asu.edu>
2017-02-26 13:57:48 -0700

    important changes

--- neuronunit/tests/rheobase_only.py
+++ neuronunit/tests/rheobase_only.py
@@ -58,9 +58,9 @@ import neuronunit.capabilities as cap
 
 
 
-vr = np.linspace(-75.0,-50.0,10)
+vr = np.linspace(-75.0,-50.0,2)
 a = np.linspace(0.015,0.045,10)
-b = np.linspace(-3.5*10E-9,-0.5*10E-9,10)
+b = np.linspace(-3.5*10E-9,-0.5*10E-9,1)
 k = np.linspace(7.0E-4-+7.0E-5,7.0E-4+70E-5,10)
 C = np.linspace(1.00000005E-4-1.00000005E-5,1.00000005E-4+1.00000005E-5,10)
 
@@ -68,7 +68,7 @@ c = np.linspace(-55,-60,10)
 d = np.linspace(0.050,0.2,10)
 v0 = np.linspace(-75.0,-45.0,10)
 vt =  np.linspace(-50.0,-30.0,10)
-vpeak =np.linspace(30.0,40.0,10)
+vpeak =np.linspace(30.0,40.0,2)
 container=[]
 
 #from neuronunit.models.reduced import ReducedModel
@@ -96,9 +96,6 @@ import quantities as pq
 units = pq.pA
 verbose=False
 
-from neuronunit.models.reduced import ReducedModel
-model = ReducedModel(get_neab.LEMS_MODEL_PATH,name='vanilla',backend='NEURON')
-model=model.load_model()
 
 def generate_prediction():
 
@@ -131,42 +128,163 @@ def generate_prediction():
 
     return prediction
 #evaluate once with a current injection at 0pA
+past=[]
+def f(ampl,model):
 
-def f(ampl):
-    lookup = {} # A lookup table global to the function below.
-
-    if float(ampl) not in lookup:
+    if float(ampl) not in model.lookup:
         current = params.copy()['injected_square_current']
-
+        print('current, previous = ',ampl,model.previous)
         uc={'amplitude':ampl}
         current.update(uc)
         current={'injected_square_current':current}
-        #print(type(model))
-        #assert model != None
-
-
+        model.run_number+=1
+        print('model run number',model.run_number)
         model.inject_square_current(current)
+        model.previous=ampl
         n_spikes = model.get_spike_count()
         verbose=True
         if verbose:
             print("Injected %s current and got %d spikes" % \
                     (ampl,n_spikes))
-            '{}{}'.format('models name: ',model.name)
-            print('models name: ',model.name)
-            print(type(model))
+        model.lookup[float(ampl)] = n_spikes
+    print('returned')
+    return model
 
-        lookup[float(ampl)] = n_spikes
-        if n_spikes==1:
-            one=True
-        else:
-            one=False
-    return (ampl,n_spikes,one,lookup)
+
 
 
 #from neuronunit.models import backends
 AMPL = 0.0*pq.pA
 DELAY = 100.0*pq.ms
 DURATION = 1000.0*pq.ms
+
+
+rheobase=None
+
+
+
+def g(m):
+    while_true=True
+    steps2 = np.linspace(-100,200,8.0)
+    steps = [ i*pq.pA for i in steps2 ]
+
+    while(while_true):
+        #for m in models:
+        for k,v in m.lookup.items():
+            ample=float(k)
+            n_spikes=v
+            if v==1:
+                print('hit rheobase')
+                while_true=False
+                m.rheobase=ample
+                return (m.rheobase,m.attrs)
+            else:
+                #if not len(sub) or len(supra):
+
+                sub = np.array([x for x in m.lookup if m.lookup[x]==0])#*units
+                supra = np.array([x for x in m.lookup if m.lookup[x]>0])#*units
+
+                step_size=abs(float(steps[1])-float(steps[0]))
+
+
+                if len(sub) and len(supra):
+                    new_search_middle=(supra.min() + sub.max())/2.0
+                    steps2 = np.linspace(new_search_middle-(step_size/2.0),new_search_middle+(step_size/2.0),8.0)
+                    steps = [ i*pq.pA for i in steps2 ]
+
+                elif len(sub):
+                    new_search_middle=(sub.max())
+                    steps2 = np.linspace(new_search_middle,new_search_middle+(step_size),8.0)
+                    steps = [ i*pq.pA for i in steps2 ]
+
+                elif len(supra):
+                    new_search_middle=(supra.min())
+                    steps2 = np.linspace(new_search_middle-(step_size),new_search_middle,6.0)
+                    steps = [ i*pq.pA for i in steps2 ]
+
+                model=f(ample,m)
+
+    #return m
+def main(iter_arg,model):
+    attrs={}
+    attrs['//izhikevich2007Cell']={}
+    param=['vr','vpeak']
+    i,j=iter_arg
+    print(i,j)
+    #model.name=str(i)+str(j)
+    model.name=str(i)+str(j)#+str(k)+str(k)
+    assert model.name != 'vanilla'
+    attrs['//izhikevich2007Cell']['vr']=i
+    attrs['//izhikevich2007Cell']['vpeak']=j#40.0
+    model.update_run_params(attrs)
+    model.rheobase=None
+    model.lookup={}
+    import pdb
+    from itertools import repeat
+    guess = float(get_neab.observation['value'])
+    past=[]
+    model=f(guess,model)
+    model=g(model)
+
+    #models=list(futures.map(steps,g))
+
+    #pdb.set_trace()
+    for k,v in model.lookup.items():
+        ample=float(k)
+        n_spikes=v
+
+
+    #print(steps)
+    from itertools import repeat
+    for k,v in model.lookup.items():
+        ample=float(k)
+        n_spikes=v
+
+    model=f(ample,model)
+
+        #model.lookup.
+    #models=list(futures.map(f,repeat(ample),repeat(model)))
+    #print(len(models))
+    #print('got here')
+
+    #models=list(futures.map(g,repeat(model))
+
+    #iter_models= [ m for m in models ]
+    #pdb.set_trace()
+
+
+
+
+
+if __name__ == "__main__":
+    import time
+    beggining=time.time()
+    score_matrix=[]
+    iter_list=[ (i,j) for i in vr for j in vpeak ]
+    from itertools import repeat
+
+    from neuronunit.models.reduced import ReducedModel
+
+    model = ReducedModel(get_neab.LEMS_MODEL_PATH,name='vanilla',backend='NEURON')
+
+    model=model.load_model()
+    gen=futures.map(main,iter_list,repeat(model))
+    #iter_models= [ m for m in models ]
+
+    for g in gen:
+        score_matrix.append((g))
+    print(len(score_matrix))
+    print(score_matrix)
+    end=time.time()
+    whole_time=end-beggining
+    f=open('brute_force_time','w')
+    f.write(str(whole_time))
+    f.close()
+    import pickle
+
+    with open('score_matrix.pickle', 'wb') as handle:
+        pickle.dump(score_matrix, handle)
+
 
 
 #def update_amplitude(test,tests):
@@ -199,155 +317,7 @@ def func2map(iter_arg,suite):
     RUN_TIMES='{}{}{}'.format('counting simulation run times on models',model.results['run_number'],model.run_number)
     return model.s_html
 '''
-rheobase=None
-def main(iter_arg):
-    attrs={}
-    attrs['//izhikevich2007Cell']={}
-    param=['a','b']
-    i,j=iter_arg
-    print(i,j)
-    #model.name=str(i)+str(j)
-    model.name=str(i)+str(j)#+str(k)+str(k)
-    assert model.name != 'vanilla'
-    if model.name!='vanilla':
 
-        print(type(model))
-
-        attrs['//izhikevich2007Cell']['a']=i
-        attrs['//izhikevich2007Cell']['b']=j
-        attrs['//izhikevich2007Cell']['vpeak']=40.0
-
-        print(attrs)
-
-        model.update_run_params(attrs)
-
-
-
-        import pdb
-        from itertools import repeat
-
-        #pdb.set_trace()
-        #first assume that the emperical observation is the right rheobase current
-        guess = float(get_neab.observation['value'])
-        #guess = 100*pq.pA
-        (ampl,n_spikes,one,lookup)=f(guess)
-        #sub = np.array([ x for x in lookup if x==0 ])#*units
-        #supra = np.array([ x for x in lookup if x>0 ])#*units
-        sub=[]
-        supra=[]
-        if n_spikes==1:
-            return ampl#*pq.pA
-        elif n_spikes==0:
-            sub=[ampl]
-            steps2 = np.linspace(ampl,ampl+150.0,8.0)
-            steps = [ i*pq.pA for i in steps2 ]
-        elif n_spikes>0:
-            supra=[ampl]
-            steps2 = np.linspace(0,ampl,8.0)
-            steps = [ i*pq.pA for i in steps2 ]
-
-
-
-        while_true=True
-        while(while_true):
-            from itertools import repeat
-            lookup2=list(futures.map(f,steps))
-            for x in lookup2:
-                if x[2]==True:
-                    print('hit rheobase')
-                    print(x)
-                    rheobase=x[0]#*pq.pA
-                    while_true=False
-                    model.rheobase=rheobase
-                    if model.name!='vanilla':
-                        return model.rheobase
-                    '''
-                    tests=get_neab.tests
-                    def update_amplitude(test,tests,score):
-                        for i in [4,5,6]:
-                            tests[i].params['injected_square_current']['amplitude'] = rheobase*1.01
-
-                    #update_amplitude.rheobase=rheobase
-                    hooks = {tests[0]:{'f':update_amplitude}} #This is a trick to dynamically insert the method
-                    #update amplitude at the location in sciunit thats its passed to, without any loss of generality.
-                    #pdb.set_trace()
-                    suite = sciunit.TestSuite("vm_suite",tests,hooks=hooks)
-                    score = suite.judge(model)#passing in model, changes model
-                    print(score)
-
-                    error = []
-                    for i in score.unstack():
-                        pdb.set_trace()
-                        assert type(i)!=None
-                        if type(i)==None:
-                            i=100
-                    error = [ abs(i.score) for i in score.unstack() ]
-                    print(error)
-                    model.s_html=(score.to_html(),attrs)
-                    print(attrs)
-
-                    model.run_number+=1
-                    RUN_TIMES='{}{}{}'.format('counting simulation run times on models',model.results['run_number'],model.run_number)
-
-                    #bg_bf=time.time()
-
-
-                    return model.s_html
-                    '''
-
-            sub = np.array([ x[0] for x in lookup2 if x[1]==0 ])#*units
-            supra = np.array([ x[0] for x in lookup2 if x[1]>0 ])#*units
-
-
-            #print(sub)
-            #print(supra)
-            step_size=abs(float(steps[1])-float(steps[0]))
-            #print(step_size)
-
-            if len(sub) and len(supra):
-                new_search_middle=(supra.min() + sub.max())/2.0
-                steps2 = np.linspace(new_search_middle-(step_size/2.0),new_search_middle+(step_size/2.0),8.0)
-                steps = [ i*pq.pA for i in steps2 ]
-
-            elif len(sub):
-                new_search_middle=(sub.max())
-                steps2 = np.linspace(new_search_middle,new_search_middle+(step_size),8.0)
-
-                steps = [ i*pq.pA for i in steps2 ]
-
-            elif len(supra):
-                new_search_middle=(supra.min())
-                steps2 = np.linspace(new_search_middle-(step_size),new_search_middle,8.0)
-
-                steps = [ i*pq.pA for i in steps2 ]
-
-
-from itertools import repeat
-iter_list=[ (i,j) for i in a for j in b ]
-def main2(ijiterator):
-    #print(ijiterator)
-    print(len(ijiterator))
-    i,j=ijiterator
-    print(i,j)
-    return(i,j)
-if __name__ == "__main__":
-    import time
-    beggining=time.time()
-    score_matrix=[]
-    gen=futures.map(main,iter_list)
-    for g in gen:
-        score_matrix.append(g)
-    print(len(score_matrix))
-    print(score_matrix)
-    end=time.time()
-    whole_time=end-beggining
-    f=open('brute_force_time','w')
-    f.write(str(whole_time))
-    f.close()
-    import pickle
-
-    with open('score_matrix.pickle', 'wb') as handle:
-        pickle.dump(score_matrix, handle)
 
     #Do the rheobase test. This is a serial bottle neck that must occur before any parallel optomization.
     #Its because the optimization routine must have apriori knowledge of what suprathreshold current injection values are for each model.
