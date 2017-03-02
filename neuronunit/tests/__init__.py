e280e158edcaead344972503bc28c449d9278af6
Russell Jarvis <russell@chaos11.la.asu.edu>
2017-03-01 17:25:44 -0700

    removed unnecessary computer score

--- neuronunit/tests/__init__.py
+++ neuronunit/tests/__init__.py
@@ -689,7 +689,7 @@ class RestingPotentialTest(VmTest):
         std = model.get_std_vm()
         prediction = {'mean':median, 'std':std}
         return prediction
-
+    '''
     def compute_score(self, observation, prediction):
         """Implementation of sciunit.Test.score_prediction."""
         #print("%s: Observation = %s, Prediction = %s" % \
@@ -702,3 +702,4 @@ class RestingPotentialTest(VmTest):
                         compute_score(observation, prediction)
             #self.bind_score(score,None,observation,prediction)
         return score
+    '''
