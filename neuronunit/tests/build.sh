785aba60088018b529f4914df82984f905336da9
Russell Jarvis <rjjarvis@asu.edu>
2017-03-06 09:28:26 -0700

    clean junk

--- neuronunit/tests/build.sh
+++
@@ -1,7 +0,0 @@
-docker build scipy-notebook-plus -t scidash/scipy-notebook-plus
-docker build neuron-mpi-neuroml -t scidash/neuron-mpi-neuroml
-docker build neuronunit -t scidash/neuronunit
-
-for stack in scipy-notebook-plus neuron-mpi-neuroml neuronunit neuronunit-docs; do
-    docker build $stack -t scidash/$stack
-done
