fced27110ce1e9a82ae73996fe9f05b8fa9cc29d
Richard C Gerkin <rick1@gerkin.org>
2017-04-02 11:27:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,4 +19,15 @@ setup(
     license='MIT',
     description='A SciUnit library for data-driven testing of single-neuron physiology models.',
     long_description="",
+    install_requires=['scipy>=0.17',
+                      'matplotlib>=1.5',
+                      'neo==0.4',
+                      'elephant',
+                      'sciunit==0.1.5.6',
+                      'allensdk==0.12.4.1',
+                      'pyneuroml>=0.2.3',
+                      'scoop'],
+    dependency_links = ['https://github.com/scidash/sciunit/tarball/dev#egg=sciunit-0.1.5.6',
+                        'https://github.com/rgerkin/AllenSDK/tarball/python3.5#egg=allensdk-0.12.4.1',
+                        'https://github.com/rgerkin/pyNeuroML/tarball/master#egg=pyneuroml-0.2.3']
 )
