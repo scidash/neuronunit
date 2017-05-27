f166c4928027ac1ee016e231a0a22d3ad4c183ce
Russell Jarvis <rjjarvis@asu.edu>
2017-05-27 15:02:09 -0700

    dev changes

--- neuronunit/tests/singularityimage/bash_process_image.sh
+++ neuronunit/tests/singularityimage/bash_process_image.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#delete old images if they exist
 if [ -f "*.img" ] ; then rm *.img;  fi
-
+#derive a singularity image from translating a docker container
 sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/singularity\image:/output --privileged -t --rm singularityware/docker2singularity pnp:latest
-sudo singularity bootstrap pnp_latest-2017-05-19-f1d9712ba440.img nu.def
+#bootstrap some bash commands into the image, they effects should be lasting because VM images can have persistance.
+sudo singularity bootstrap pnp*.img nu.def
+#Add the required files.
+sudo singularity exec pnp*.img python 
 
+sudo rsync pnp*.img rjjarvis@comet.sdsc.xsede.org:~/
+#boot strap file no longer required, as bootstrapping is done locally.
+#sudo scp nu.def rjjarvis@comet.sdsc.xsede.org:~/
