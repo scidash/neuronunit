99b99f27e4396065c2ae5d1e9886da3ae9f6ed59
Russell Jarvis <rjjarvis@asu.edu>
2017-05-27 15:07:31 -0700

    dev changes
    Former-commit-id: 601b67130072ebdc8c0fc478a975acc6a06febaa

--- neuronunit/tests/singularityimage/bash_process_image.sh
+++ neuronunit/tests/singularityimage/bash_process_image.sh
@@ -5,6 +5,14 @@ if [ -f "*.img" ] ; then rm *.img;  fi
 #derive a singularity image from translating a docker container
 sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/singularity\image:/output --privileged -t --rm singularityware/docker2singularity pnp:latest
 #bootstrap some bash commands into the image, they effects should be lasting because VM images can have persistance.
+#This line achieves same as:
+#git checkout -b development https://github.com/singularityware/singularity
+#git pull
+#./autogen.sh
+#./configure --prefix=/usr/local
+#make
+#sudo make install
+
 sudo singularity bootstrap pnp*.img nu.def
 #Add the required files.
 sudo singularity exec pnp*.img python 
@@ -12,3 +20,15 @@ sudo singularity exec pnp*.img python
 sudo rsync pnp*.img rjjarvis@comet.sdsc.xsede.org:~/
 #boot strap file no longer required, as bootstrapping is done locally.
 #sudo scp nu.def rjjarvis@comet.sdsc.xsede.org:~/
+
+
+if [ -d "~/singularity" ];
+    git checkout -b development https://github.com/singularityware/singularity
+    cd singularity
+    git pull
+    ./autogen.sh
+    ./configure --prefix=/usr/local
+    make
+    sudo make install;
+fi
+
