1cf1ec6c44120f6fe7fdc56138dfb70e7133b877
Richard C Gerkin <rgerkin@asu.edu>
2017-05-26 21:05:54 -0700

    Fixes for raw score handling
    Former-commit-id: f2bb641726f9c177cbd8f10806861e8b487070d5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='neuronunit',
-    version='0.1.8.6',
+    version='0.1.8.7',
     author='Rick Gerkin',
     author_email='rgerkin@asu.edu',
         packages=[
@@ -23,7 +23,7 @@ setup(
                       'matplotlib>=1.5',
                       'neo==0.4',
                       'elephant',
-                      'sciunit==0.1.5.6',
+                      'sciunit==0.1.5.7',
                       'allensdk==0.12.4.1',
                       'pyneuroml>=0.2.3',
                       'scoop'],
