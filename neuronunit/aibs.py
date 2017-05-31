ddf2fae3de658234084f64064687e21f63ba73a0
Richard C Gerkin <rgerkin@asu.edu>
2017-05-31 15:17:53 -0700

    Improved data download caching and error handling

--- neuronunit/aibs.py
+++ neuronunit/aibs.py
@@ -8,6 +8,11 @@ from allensdk.api.queries.cell_types_api import CellTypesApi
 
     
 def get_sweep_params(dataset_id, sweep_id):
+    """
+    Gets sweep parameters corresponding to the sweep with id 'sweep_id' from
+    the dataset with id 'dataset_id'.
+    """
+
     ct = CellTypesApi()
     experiment_params = ct.get_ephys_sweeps(dataset_id)
     sp = None
@@ -21,11 +26,13 @@ def get_sweep_params(dataset_id, sweep_id):
 
 
 def get_sp(experiment_params, sweep_ids):
-    '''
-    get sweep parameter. A candidate method for replacing get_sweep_params.
-    This fix is necessary due to changes in the allensdk however:
-    Warning. This method may not properly convey the meaning of get_sweep_params
-    '''
+    """
+    A candidate method for replacing 'get_sweep_params'.
+    This fix is necessary due to changes in the allensdk.
+    Warning: This method may not properly convey the original meaning 
+    of 'get_sweep_params'.
+    """
+    
     sp = None
     for sp in experiment_params:
         for sweep_id in sweep_ids:
@@ -38,11 +45,12 @@ def get_sp(experiment_params, sweep_ids):
 
 
 def get_observation(dataset_id, kind, cached=True, quiet=False):
-    if cached:
-        db = shelve.open('aibs-cache')
-    else:
-        db = {}
-
+    """
+    Gets an observation of kind 'kind' from the dataset with id 'dataset_id', 
+    optionally using the cached value retrieved previously.
+    """
+    
+    db = shelve.open('aibs-cache') if cached else {}
     identifier = '%d_%s' % (dataset_id,kind)
     if identifier in db:
         print("Getting %s cached data value for from AIBS dataset %s" \
@@ -62,15 +70,19 @@ def get_observation(dataset_id, kind, cached=True, quiet=False):
             value *= pq.pA # Apply units.
         db[identifier] = value
     
+    if cached:
+        db.close()
     return {'value': value}
 
        
 def get_value_dict(experiment_params, sweep_ids, kind):
-    '''
-    A candidate method for replacing get_observation.
-    This fix is necessary due to changes in the allensdk however:
-    Warning. Togethor with get_sp this method may not properly convey the meaning of get_observation
-    '''
+    """
+    A candidate method for replacing 'get_observation'.
+    This fix is necessary due to changes in the allensdk.
+    Warning: Together with 'get_sp' this method may not properly 
+    convey the meaning of 'get_observation'.
+    """
+
     if kind == str('rheobase'):
         sp = get_sp(experiment_params,sweep_ids)
         value = sp['stimulus_absolute_amplitude']
