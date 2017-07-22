3db372e502ebda5765c2ece85af1e5470cd88fd1
Russell Jarvis <colouredstatic@gmail.com>
2017-07-22 13:48:27 +1000

    Remove unintentional duplicated code block.
    Remove duplicated code block.
    
    Former-commit-id: 3c17e250f44196c52429cd51ffb6e091e267ded1

--- neuronunit/models/backends.py
+++ neuronunit/models/backends.py
@@ -34,25 +34,6 @@ class HasSegment(sciunit.Capability):
 
         return self.section(self.location)
 
-
-class HasSegment(sciunit.Capability):
-    """Model has a membrane segment of NEURON simulator"""
-
-    def setSegment(self, section, location = 0.5):
-        """Sets the target NEURON segment object
-        section: NEURON Section object
-        location: 0.0-1.0 value that refers to the location
-        along the section length. Defaults to 0.5
-        """
-
-        self.section = section
-        self.location = location
-
-    def getSegment(self):
-        """Returns the segment at the active section location"""
-
-        return self.section(self.location)
-
 class Backend:
     """Base class for simulator backends that implement simulator-specific
     details of modifying, running, and reading results from the simulation
