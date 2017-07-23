214663afdea737324e91dd2296a30118a6becd59
Richard C Gerkin <rick1@gerkin.org>
2017-07-23 14:21:19 -0700

    Update setup.py
    Former-commit-id: 72a5ac4015762792bc07fefa17b8d4d9c28ce97e

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description="",
     install_requires=['scipy>=0.17',
                       'matplotlib>=2.0',
-                      'neo==0.5.1',
+                      'neo==9999',
                       'elephant==0.4.1',
                       'igor==0.3',
                       'sciunit==9999',
@@ -29,5 +29,6 @@ setup(
                       'pyneuroml==9999'],
     dependency_links = ['git+https://github.com/scidash/sciunit@dev#egg=sciunit-9999',
                         'git+https://github.com/rgerkin/AllenSDK@master#egg=allensdk-9999',
-                        'git+https://github.com/rgerkin/pyNeuroML@master#egg=pyneuroml-9999']
+                        'git+https://github.com/rgerkin/pyNeuroML@master#egg=pyneuroml-9999',
+                        'git+https://github.com/rgerkin/python-neo@master#egg=neo-9999']
 )
