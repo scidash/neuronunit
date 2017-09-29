8e10c215cca036baaaeec4bb3939aeb26447fa78
Richard C Gerkin <rgerkin@asu.edu>
2017-09-28 20:45:31 -0700

    Added test skipping when remote URLs are not working
    Former-commit-id: 1df1bbbea483e2ee8d26c67eea6fc03af5f8c9de

--- unit_test/core_tests.py
+++ unit_test/core_tests.py
@@ -11,12 +11,21 @@ import os
 import warnings
 import pdb
 
+try: # Python 2
+    from urllib import urlretrieve
+except ImportError: # Python 3
+    from urllib.request import urlretrieve
+
+if sys.platform == 'darwin':
+    import matplotlib as mpl
+    mpl.use('Agg')
+
 from sciunit.utils import NotebookTools
+from neuronunit import neuroelectro,bbp
 
-# CONVERT_NOTEBOOKS environment variable controls whether notebooks are
-# executed as notebooks or converted to regular python files first. 
 
-class DocumentationTestCase(NotebookTools,unittest.TestCase):
+class DocumentationTestCase(NotebookTools, 
+                            unittest.TestCase):
     """Testing documentation notebooks"""
 
     path = '../docs'
@@ -25,7 +34,8 @@ class DocumentationTestCase(NotebookTools,unittest.TestCase):
     def test_chapter1(self):
         self.do_notebook('chapter1')
 
-class EphysPropertiesTestCase(NotebookTools,unittest.TestCase):
+class EphysPropertiesTestCase(NotebookTools, 
+                              unittest.TestCase):
     """Testing sciunit tests of ephys properties"""
 
     path = '.'
@@ -38,30 +48,33 @@ class EphysPropertiesTestCase(NotebookTools,unittest.TestCase):
 class NeuroElectroTestCase(unittest.TestCase):
     """Testing retrieval of data from NeuroElectro.org"""
 
+    @unittest.skipUnless(neuroelectro.is_neuroelectro_up(), 
+                         "NeuroElectro URL is not responsive")
     def test_neuroelectro(self):
-        from neuronunit.neuroelectro import NeuroElectroDataMap,\
-                                            NeuroElectroSummary
-        x = NeuroElectroDataMap()
+        x = neuroelectro.NeuroElectroDataMap()
         x.set_neuron(id=72)
         x.set_ephysprop(id=2)
         x.set_article(pmid=18667618)
         x.get_values()
         x.check()
 
-        x = NeuroElectroSummary()
+        x = neuroelectro.NeuroElectroSummary()
         x.set_neuron(id=72)
         x.set_ephysprop(id=2)
         x.get_values()
         x.check()
         
 
-class BlueBrainTestCase(NotebookTools,unittest.TestCase):
+class BlueBrainTestCase(NotebookTools,
+                        unittest.TestCase):
      
     path = '.'
     
+    @unittest.skipUnless(bbp.is_bbp_up(), 
+                         "Blue Brain URL is not responsive")
     def test_bluebrain(self):
         self.do_notebook('bbp')
         
-        
+
 if __name__ == '__main__':
     unittest.main()
