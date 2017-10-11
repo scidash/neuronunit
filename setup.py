d036528424283ecca5d3c9aa81f41faefd827bef
rgerkin <rgerkin@asu.edu>
2017-10-10 22:16:04 -0700

    Set up unit test for coveralls

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='neuronunit',
-    version='0.1.8.9',
+    version='0.19',
     author='Rick Gerkin',
     author_email='rgerkin@asu.edu',
         packages=[
@@ -13,11 +13,13 @@ setup(
             'neuronunit.capabilities',
             'neuronunit.neuroconstruct',
             'neuronunit.models',
-            'neuronunit.tests'],
+            'neuronunit.tests',
+            'neuronunit.unit_test'],
     url='http://github.com/scidash/neuronunit',
     license='MIT',
     description='A SciUnit library for data-driven testing of single-neuron physiology models.',
     long_description="",
+    test_suite="neuronunit.unit_test.core_tests",    
     install_requires=['scipy>=0.17',
                       'matplotlib>=2.0',
                       'neo==0.5.2',
@@ -27,12 +29,13 @@ setup(
                       'sciunit==9999',
                       'allensdk==0.14.2',
                       #'allensdk==9999',
-                      'pyneuroml==0.3.1',
+                      #'pyneuroml==0.3.1',
                       #'pyneuroml==9999'
                       ],
+    #test_requires=[],
     dependency_links = ['git+https://github.com/scidash/sciunit@dev#egg=sciunit-9999',
                         #'git+https://github.com/rgerkin/AllenSDK@master#egg=allensdk-9999',
-                        #'git+https://github.com/rgerkin/pyNeuroML@master#egg=pyneuroml-9999',
+                        'git+https://github.com/rgerkin/pyNeuroML@master#egg=pyneuroml-9999',
                         #'git+https://github.com/rgerkin/python-neo@master#egg=neo-9999',
                         ]
 )
