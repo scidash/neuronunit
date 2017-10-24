c9cf0277870abd111933545a1da5d6862c2384eb
Russell Jarvis <rjjarvis@asu.edu>
2017-10-23 22:02:23 -0700

    renamed test appropriately, and some object orientated interface to optimizer

--- unit_test/testNEURONparallel.py
+++ unit_test/testNEURONparallel.py
@@ -10,21 +10,6 @@ rc = ipp.Client(profile='default')
 rc[:].use_cloudpickle()
 dview = rc[:]
 
-#import datetime
-#import pytz
-
-#from datetime import datetime
-#from datetime import timezone
-
-#dt = datetime.now()
-#dt.replace(tzinfo=timezone.utc)
-
-#print(dt.replace(tzinfo=timezone.utc).isoformat())
-#import warnings
-#warnings.simplefilter('off')
-
-#class TestsTestCase(object):
-#    """Abstract base class for testing tests"""
 class TestBackend(unittest.TestCase):
 
 
@@ -33,8 +18,7 @@ class TestBackend(unittest.TestCase):
         self.predictionp = None
         self.score_p = None
         self.score_s = None
-        #self.timed_p = None
-        #self.timed_s = None
+
 
     def get_observation(self, cls):
         print(cls.__name__)
@@ -82,10 +66,35 @@ class TestBackend(unittest.TestCase):
         self.assertEqual(booleans, booleanp[0])
 
 
+    def test_5ngsa(self):
+        from neuronunit.optimization import nsga_object
 
-    def test_5_data_transport_containers(self):
-        MU = 200
+        #N = nsga_object.NSGA(10,10,10)
+        #invalid_dtc, pop, logbook, fitnesses = N.main(4,2)
+        import numpy as np
+        N = nsga_object.NSGA()
+        NGEN = 3
+        MU = 4
+
+        invalid_dtc, pop, logbook, fitnesses = N.main(MU,NGEN)
+        pf = np.mean(fitnesses)
+        NGEN = 3
+        MU = len(pop)
+
+        for gen in range(1, NGEN):
+            final_dtc, pop, final_logbook, final_fitnesses = N.evolve(pop,MU,gen)
+        final_pop = pop
+        ff = np.mean(final_fitnesses)
+        import pdb; pdb.set_trace()
+
+        self.assertNotEqual(ff,pf)
+        self.assertGreater(ff,pf)
+
+
+    def test_6_data_transport_containers_on_bulk(self):
+        MU = 10000
         import deap
+        import numpy as np
         import copy
         from neuronunit.optimization import evaluate_as_module
         #from neuronunit.optimization import model_parameters
@@ -113,11 +122,42 @@ class TestBackend(unittest.TestCase):
         for index, dtc in enumerate(dtcpop):
             if index!=0:
                 self.assertNotEqual(old_attrs, dtc.attrs)
+                print(old_attrs,dtc.attrs)
             old_attrs = dtc.attrs
+
+        for index, dtc in enumerate(dtcpop):
+            value = dtc.attrs['b']
+            boolean = np.isnan(float(value))
+            self.assertEqual(boolean, False)
+            print(boolean)
+
+            #import pdb; pdb.set_trace()
         # use set to verify the set of attrs is unique.
         self.assertEqual(len(dtcpop), len(pop))
+        return dtcpop, pop
 
-    def test_6rheobase_setup(self):
+    def serial_7rheobase(self):
+        from neuronunit.tests.fi import RheobaseTest, RheobaseTestP
+        from neuronunit.optimization import get_neab
+        from neuronunit.models.reduced import ReducedModel
+        from neuronunit import aibs
+        import os
+        dataset_id = 354190013  # Internal ID that AIBS uses for a particular Scnn1a-Tg2-Cre
+                                # Primary visual area, layer 5 neuron.
+        observation = aibs.get_observation(dataset_id,'rheobase')
+        rt = RheobaseTest(observation = observation)
+        model = ReducedModel(get_neab.LEMS_MODEL_PATH,name=str('vanilla'),backend='NEURON')
+        self.score_s = rt.judge(model,stop_on_error = True, deep_error = True)
+        self.predictions = self.score_s.prediction
+        self.score_s = self.score_s.sort_key
+        #print(' serial score {0} parallel score {1}'.format(self.score_s,self.score_p))
+
+        self.assertNotEqual(type(self.scores_s),type(None))
+        #self.assertEqual(int(self.score_s*1000), int(self.score_p*1000))
+        #self.assertEqual(int(self.predictionp['value']), int(self.predictions['value']))
+
+
+    def test_8rheobase_setup(self):
         from neuronunit.tests.fi import RheobaseTest, RheobaseTestP
         from neuronunit.optimization import get_neab
         from neuronunit.models.reduced import ReducedModel
@@ -136,8 +176,59 @@ class TestBackend(unittest.TestCase):
         self.predictions = self.score_s.prediction
         self.score_s = self.score_s.sort_key
         print(' serial score {0} parallel score {1}'.format(self.score_s,self.score_p))
-        #self.assertEqual(int(self.score_s*1000), int(self.score_p*1000))
-        #self.assertEqual(int(self.predictionp['value']), int(self.predictions['value']))
+        self.assertEqual(int(self.score_s*1000), int(self.score_p*1000))
+        self.assertEqual(int(self.predictionp['value']), int(self.predictions['value']))
+
+    def test_9ngsa_setup(self):
+        dtcpop, pop = self.test_6_data_transport_containers_on_bulk()
+        dtcpop = dtcpop[0:9]#.extend(dtcpop[-5:-1])
+        pop = pop[0:9]#.extend(pop[-5:-1])
+        #dtcpop =
+        #pop = pop[-5:-1]
+        #dtcpop = nsga_parallel.dtc_to_rheo(dtcpop)
+        from neuronunit.optimization import model_parameters as modelp
+        from neuronunit.optimization import evaluate_as_module
+        from neuronunit.optimization import nsga_parallel
+        update_dtc_pop = evaluate_as_module.update_dtc_pop
+        pre_format = evaluate_as_module.pre_format
+        dtc_to_rheo = nsga_parallel.dtc_to_rheo
+        map_wrapper = nsga_parallel.map_wrapper
+        pre_size = len(dtcpop)
+        #dtcpop = update_dtc_pop(pop, td)
+        dtcpop = list(map(dtc_to_rheo,dtcpop))
+        post_size = len(dtcpop)
+        self.assertEqual(pre_size,post_size)
+
+        dtcpop = list(map(pre_format,dtcpop))
+        post_size = len(dtcpop)
+        self.assertEqual(pre_size,post_size)
+
+        dtcpop = list(dview.map_sync(map_wrapper,dtcpop))
+        post_size = len(dtcpop)
+        self.assertEqual(pre_size,post_size)
+
+        return pop,dtcpop
+
+
+    def test_10ngsa(self):
+        pop,dtcpop = self.test_8ngsa_setup()
+
+    def test_11(self):
+        from neuronunit.optimization import nsga_parallel
+        from neuronunit.optimization import evaluate_as_module
+
+        for i in range(0,10):
+            subset = nsga_parallel.create_subset(nparams=i)
+            numb_err_f = 8
+            toolbox, tools, history, creator, base = evaluate_as_module.import_list(ipp,subset,numb_err_f)
+            ind = toolbox.population(n = 1)
+            self.assertEqual(len(ind)+1,i)
+            #pop = [ toolbox.clone(i) for i in pop ]
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
     '''
