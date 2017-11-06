2b2b10200cd20cc80f55b5ae3e3e6779086124c3
Russell Jarvis <rjjarvis@asu.edu>
2017-11-05 20:06:40 -0700

    more functional

--- unit_test/test_exhaustive_search.py
+++ unit_test/test_exhaustive_search.py
@@ -4,10 +4,10 @@
 NGEN = 3
 CXPB = 0.9
 MU = 6; NGEN = 4; CXPB = 0.9
-nparam = 10
 
-from neuronunit.optimization import nsga_parallel as nsgap
-
+#from neuronunit.optimization import nsga_parallel as nsgap
+from neuronunit.optimization import exhaustive_search as es
+'''
 ###
 # less than 10 params currently unsupported.
 difference_progress, fitnesses, pf, logbook, pop, dtcpop, stats, scores_nsgaW = nsgap.main(MU=MU, \
@@ -17,10 +17,12 @@ difference_progress, fitnesses, pf, logbook, pop, dtcpop, stats, scores_nsgaW = 
 import pickle
 with open('complete_dump.p','wb') as handle:
    pickle.dump([difference_progress, fitnesses, pf, logbook, pop, dtcpop, stats, scores_nsga],handle)
-
 lists = pickle.load(open('complete_dump.p','rb'))
+'''
+
 #dtcoffspring2,history2,logbook2 = lists[0],lists[1],lists[2]
-from neuronunit.optimization import exhaustive_search as es
 npoints = 3
-nparams = 10
+nparams = 2
 scores_exh, dtcpop = es.run_grid(npoints,nparams)
+print(dtcpop)
+print(scores_exh)
