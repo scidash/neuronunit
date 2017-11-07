a1aa186224873f9bd1596096558d6430955468f3
Russell Jarvis <colouredstatic@gmail.com>
2017-11-06 18:43:54 -0700

    Update test_exhaustive_search.py

--- unit_test/test_exhaustive_search.py
+++ unit_test/test_exhaustive_search.py
@@ -21,8 +21,8 @@ lists = pickle.load(open('complete_dump.p','rb'))
 '''
 
 #dtcoffspring2,history2,logbook2 = lists[0],lists[1],lists[2]
-npoints = 2
-nparams = 2
+npoints = 3
+nparams = 3
 dtcpop = es.run_grid(npoints,nparams)
 for d in dtcpop:
     print(d.scores)
