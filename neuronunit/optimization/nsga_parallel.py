e4868805f770cfc4d76e0a0aac5177ba2141fb6e
Russell Jarvis <colouredstatic@gmail.com>
2017-11-30 16:46:55 -0700

    adding some brian2 to backends, adding DTC to model constructor, removing get_neab reads and writes whereever possible by systematic pushing and pulling from target=0

--- neuronunit/optimization/nsga_parallel.py
+++ neuronunit/optimization/nsga_parallel.py
@@ -14,8 +14,6 @@ import numpy as np
 import ipyparallel as ipp
 rc = ipp.Client(profile='default')
 #rc.Client.become_dask()
-
-rc[:].use_cloudpickle()
 dview = rc[:]
 
 
@@ -24,14 +22,52 @@ dview = rc[:]
 from ipyparallel import depend, require, dependent
 # Import get_neab has to happen exactly here. It has to be called only on
 # controller (rank0, it has)
-from neuronunit.optimization import get_neab
 from neuronunit import tests
+from neuronunit.optimization import get_neab
+tests = get_neab.tests
+
+
+
+def file_write(tests):
+    import pickle
+    with open('ne_pickle', 'wb') as f:
+        pickle.dump(tests, f)
+
+dview.block = True
+test_container = { 'tests':tests }
+dview.push(test_container)
+dview.execute('print(tests)')
+dview.execute("import pickle")
+with dview.sync_imports():
+    import pickle
+
+# serial file write.
+rc[0].apply_sync(file_write, tests)
+#Broadcast the tests to workers
+test_dic = {}
+for t in tests:
+    test_dic[str(t.name)] = t
+dview.push(test_dic)
+
+def get_tests():
+    '''
+    Pull tests
+    '''
+    tests = []
+    tests.append(dview.pull('InputResistanceTest',targets=0).get())
+    tests.append(dview.pull('TimeConstantTest',targets=0).get())
+    tests.append(dview.pull('CapacitanceTest',targets=0).get())
+    tests.append(dview.pull('RestingPotentialTest',targets=0).get())
+    tests.append(dview.pull('InjectedCurrentAPWidthTest',targets=0).get())
+    tests.append(dview.pull('InjectedCurrentAPAmplitudeTest',targets=0).get())
+    tests.append(dview.pull('InjectedCurrentAPThresholdTest',targets=0).get())
+    return tests
 
 def dtc_to_rheo(dtc):
     from neuronunit.models.reduced import ReducedModel
-    from neuronunit.optimization import get_neab
+    #from neuronunit.optimization import get_neab
     from neuronunit.optimization import evaluate_as_module
-    model = ReducedModel(get_neab.LEMS_MODEL_PATH,name=str('vanilla'),backend='NEURON')
+    model = ReducedModel(dtc.LEMS_MODEL_PATH,name=str('vanilla'),backend='NEURON')
 
     model.set_attrs(dtc.attrs)
     #if not hasattr(model,'rheobase'):
@@ -42,7 +78,7 @@ def dtc_to_rheo(dtc):
     dtc.scores = {}
     dtc.differences = None
     dtc.differences = {}
-    #get_neab.tests[0]
+    tests = get_tests()
     get_neab.tests[0].dview = dview
     score = get_neab.tests[0].judge(model,stop_on_error = False, deep_error = True)
     observation = score.observation
@@ -80,7 +116,6 @@ def evaluate(dtc):
         else:
             fitness[k] = -100.0
 
-    #print(fitness)
     return fitness[0],fitness[1],\
            fitness[2],fitness[3],\
            fitness[4],fitness[5],\
@@ -111,12 +146,10 @@ def update_pop(pop,td):
     print('\n\n\n\n preformat complete \n\n\n\n')
     dtcpop = dview.map(parallel_method,dtcpop).get()
     rc.wait(dtcpop)
-    #dtcpop=list(dtcpop)
-    #dtcpop = list(dview.map_sync(parallel_method,dtcpop))
+
     print('\n\n\n\n score calculation complete \n\n\n\n')
     return list(dtcpop)
-    #import copy
-    #return copy.copy(dtcpop)
+
 
 
 def create_subset(nparams=10,provided_keys=None):
@@ -134,138 +167,3 @@ def create_subset(nparams=10,provided_keys=None):
 
     subset = { k:mp[k] for k in reduced_key_list }
     return subset
-'''
-Deprecated
-def main(MU=12, NGEN=4, CXPB=0.9, nparams=10):
-    import deap
-    import copy
-
-    from neuronunit.optimization import evaluate_as_module
-    from neuronunit.optimization import model_parameters
-
-    scores = []
-
-    subset = create_subset(nparams=10)
-    numb_err_f = 8
-    toolbox, tools, history, creator, base = evaluate_as_module.import_list(ipp,subset,numb_err_f)
-
-    dview.push({'Individual':evaluate_as_module.Individual})
-    dview.apply_sync(evaluate_as_module.import_list,ipp,subset,numb_err_f)
-    get_trans_dict = evaluate_as_module.get_trans_dict
-    td = get_trans_dict(subset)
-    dview.push({'td':td })
-
-    pop = toolbox.population(n = MU)
-    pop = [ toolbox.clone(i) for i in pop ]
-    dview.scatter('Individual',pop)
-
-    dtcpop = update_pop(pop,td)
-    assert len(dtcpop) == len(pop)
-
-    #for dtc in dtcpop:
-    #    scores.append(dtc.scores)
-    fitnesses = list(dview.map_sync(evaluate,dtcpop))
-    assert len(fitnesses) == len(pop)
-
-    print(dtcpop,fitnesses)
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
-
-    pop = tools.selNSGA2(pop, MU)
-
-    # only update the history after crowding distance has been assigned
-    deap.tools.History().update(pop)
-    # After an evaluation of error its appropriate to display error statistics
-    pf = tools.ParetoFront()
-    pf.update([toolbox.clone(i) for i in pop])
-
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("min", np.min, axis=0)
-    stats.register("max", np.max, axis=0)
-    stats.register("avg", np.mean, axis=0)
-
-    logbook = tools.Logbook()
-    logbook.header = "gen", "evals", "std", "min", "avg", "max"
-
-    record = stats.compile(pop)
-    logbook.record(gen=0, evals=len(pop), **record)
-    means = np.array(logbook.select('avg'))
-    difference_progress = []
-    gen = 1
-    difference_progress.append(np.mean([v for dtc in dtcpop for v in dtc.differences.values()  ]))
-
-    verbose = True
-    difference_progress = []
-    while (gen < NGEN):# and means[-1] > 0.05):
-        # Although the hypervolume is not actually used here, it can be used
-        # As a terminating condition.
-        # hvolumes.append(hypervolume(pf))
-        gen += 1
-        print(gen)
-        offspring = tools.selNSGA2(pop, len(pop))
-        offspring = [toolbox.clone(ind) for ind in offspring]
-
-        for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() <= CXPB:
-                toolbox.mate(ind1, ind2)
-            toolbox.mutate(ind1)
-            toolbox.mutate(ind2)
-            # deleting the fitness values is what renders them invalid.
-            # The invalidness is used as a flag for recalculating them.
-            # Their fitneess needs deleting since the attributes which generated these values have been mutated
-            # and hence they need recalculating
-            # Mutation also implies breeding, if a gene is mutated it was also recently recombined.
-            del ind1.fitness.values, ind2.fitness.values
-
-        invalid_ind = []
-        for ind in offspring:
-            if ind.fitness.valid == False:
-                invalid_ind.append(ind)
-        # Need to make sure that _pop does not replace instances of the same model
-        # Thus waisting computation.
-
-        dtcpop = update_pop(invalid_ind,td)
-        assert len(dtcpop) == len(invalid_ind)
-
-        for dtc in dtcpop:
-            scores.append(dtc.scores)
-        fitnesses = list(dview.map_sync(evaluate,dtcpop))
-        assert len(dtcpop) == len(fitnesses)
-
-        difference_progress.append(np.mean([v for dtc in dtcpop for v in dtc.differences.values()  ]))
-        mf = np.mean(fitnesses)
-
-
-        for ind, fit in zip(copy.copy(invalid_ind), fitnesses):
-            ind.fitness.values = fit
-
-        # Its possible that the offspring are worse than the parents of the penultimate generation
-        # Its very likely for an offspring population to be less fit than their parents when the pop size
-        # is less than the number of parameters explored. However this effect should stabelize after a
-        # few generations, after which the population will have explored and learned significant error gradients.
-        # Selecting from a gene pool of offspring and parents accomodates for that possibility.
-        # There are two selection stages as per the NSGA example.
-        # https://github.com/DEAP/deap/blob/master/examples/ga/nsga2.py
-
-        #pop = toolbox.select(pop + offspring, MU)
-
-
-        pop = tools.selNSGA2(offspring + pop, MU)
-        #assert len(dtcpop) == len(pop)
-
-        record = stats.compile(pop)
-        history.update(pop)
-        logbook.record(gen=gen, evals=len(pop), **record)
-        pf.update([toolbox.clone(i) for i in pop])
-        means = np.array(logbook.select('avg'))
-        pf_mean = np.mean([ i.fitness.values for i in pf ])
-        #return difference_progress, fitnesses, pf, logbook, pop, dtcpop, stats
-
-        # if the means are not decreasing at least as an overall trend something is wrong.
-        print('means from logbook: {0} from manual meaning the fitness: {1}'.format(means,mf))
-        print('means: {0} pareto_front first: {1} pf_mean {2}'.format(logbook.select('avg'), \
-                                                            np.sum(np.mean(pf[0].fitness.values)),\
-                                                            pf_mean))
-        if gen==NGEN:
-            return difference_progress, fitnesses, pf, logbook, pop, dtcpop, stats, scores
-'''
