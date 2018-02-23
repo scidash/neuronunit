217bd6918be3cfa7e3b4466139da122bc653de0e
Russell Jarvis <rjjarvis@asu.edu>
2018-02-22 18:24:07 -0700

    unit testing progress

--- neuronunit/unit_test/test_auxillary.py
+++ neuronunit/unit_test/test_auxillary.py
@@ -7,6 +7,38 @@ import dask
 from dask import bag
 from base import *
 
+
+def grid_points():
+    npoints = 2
+    nparams = 10
+    from neuronunit.optimization.model_parameters import model_params
+    provided_keys = list(model_params.keys())
+    USE_CACHED_GS = False
+    from neuronunit.optimization import exhaustive_search
+    grid_points = exhaustive_search.create_grid(npoints = npoints,nparams = nparams)
+    import dask.bag as db
+    b0 = db.from_sequence(grid_points[0:2], npartitions=8)
+    dtcpop = list(db.map(exhaustive_search.update_dtc_grid,b0).compute())
+    assert dtcpop is not None
+    return dtcpop
+
+def test_01a_compute_score(dtcpop):
+    from neuronunit.optimization import get_neab
+    from neuronunit.optimization.optimization_management import dtc_to_rheo
+    from neuronunit.optimization.optimization_management import nunit_evaluation
+    from neuronunit.optimization.optimization_management import format_test
+    #dtcpop = grid_points()
+    dtclist = list(map(dtc_to_rheo,dtcpop))
+    for d in dtclist:
+        assert len(list(d.attrs.values())) > 0
+    import dask.bag as db
+    b0 = db.from_sequence(dtclist, npartitions=8)
+    dtclist = list(db.map(format_test,b0).compute())
+
+    b0 = db.from_sequence(dtclist, npartitions=8)
+    dtclist = list(db.map(nunit_evaluation,b0).compute())
+    return dtclist
+
 class TestBackend(NotebookTools,unittest.TestCase):
 
     def setUp(self):
@@ -14,9 +46,12 @@ class TestBackend(NotebookTools,unittest.TestCase):
         self.predictionp = None
         self.score_p = None
         self.score_s = None
-        self.rheobase = None
-        self.dtc = None
-        self.dtcpop = None
+        self.grid_points = grid_points()
+        dtcpop = self.grid_points
+        self.test_01a_compute_score = test_01a_compute_score
+        self.dtcpop = test_01a_compute_score(dtcpop)
+        self.dtc = self.dtcpop[0]
+        self.rheobase = self.dtc.rheobase
         from neuronunit.models.reduced import ReducedModel
         from neuronunit.optimization import get_neab
         self.model = ReducedModel(get_neab.LEMS_MODEL_PATH, backend='NEURON')
@@ -33,111 +68,137 @@ class TestBackend(NotebookTools,unittest.TestCase):
         return score
 
 
+    # Get experimental electro physology bservations for a dentate gyrus baskett cell
+    # An inhibitory neuron
+    @unittest.skip("takes internet resources")
+    def test_get_inhibitory_neuron(self):
+        from neuronunit import tests as nu_tests, neuroelectro
+        from neuronunit.tests import passive, waveform, fi
+        #neuron = {'nlex_id': 'nifext_50'} # Layer V pyramidal cell
+        fi_basket = {'nlex_id':'NLXCELL:100201'}
+        observation =  cls.neuroelectro_summary_observation(fi_basket)
+        test_class_params = [(fi.RheobaseTest,None),
+
+                         (passive.InputResistanceTest,None),
+                         (passive.TimeConstantTest,None),
+                         (passive.CapacitanceTest,None),
+                         (passive.RestingPotentialTest,None),
+                         (waveform.InjectedCurrentAPWidthTest,None),
+                         (waveform.InjectedCurrentAPAmplitudeTest,None),
+                         (waveform.InjectedCurrentAPThresholdTest,None)]#,
+        inh_observations = []
+        for cls,params in test_class_params:
+            inh_observations.append(cls.neuroelectro_summary_observation(fi_basket))
+        self.inh_observations = inh_observations
+        return inh_observations
 
 
-    def test_0_grid_points(self):
-        npoints = 2
-        nparams = 10
-        from neuronunit.optimization.model_parameters import model_params
-        provided_keys = list(model_params.keys())
-        USE_CACHED_GS = False
-        from neuronunit.optimization import exhaustive_search
-        self.grid_points = grid_points = exhaustive_search.create_grid(npoints = npoints,nparams = nparams)
-        import dask.bag as db
-        b0 = db.from_sequence(grid_points[0:2], npartitions=8)
-        self.dtcpop = list(db.map(exhaustive_search.update_dtc_grid,b0).compute())
-        self.dtc = self.dtcpop[0]
-        #self.dtcpop = dlist[0:2]#int(len(dlist)/N)]
-
-    def test_1_compute_score(self):
-        from neuronunit.optimization import get_neab
-        from neuronunit.optimization.optimization_management import dtc_to_rheo
-        from neuronunit.optimization.optimization_management import nunit_evaluation
-        from neuronunit.optimization.optimization_management import format_test
-
-        dtclist = list(map(dtc_to_rheo,self.dtcpop))
-        import dask.bag as db
-        b0 = db.from_sequence(dtclist, npartitions=8)
-        dtclist = list(db.map(format_test,b0).compute())
-
-        b0 = db.from_sequence(dtclist, npartitions=8)
-        dtclist = list(db.map(nunit_evaluation,b0).compute())
-        return dtclist
     #processed_dtclist = compute_chunk(self.dlist)
 
 
-    def test_4rheobase(self):
+    def test_rheobase(self):
+        from neuronunit.models.reduced import ReducedModel
+        from neuronunit.optimization import get_neab
         import copy
         import unittest
 
         dtc = copy.copy(self.dtc)
+        self.assertNotEqual(self.dtc,None)
+
         dtc.scores = {}
         size = len(list(dtc.attrs.values()))
-        assertGreat(size,0)
-
+        #@self.assertGreater(
+        assert size > 0
         model = ReducedModel(get_neab.LEMS_MODEL_PATH, name= str('vanilla'), backend=('NEURON', {'DTC':dtc}))
-        model.set_attrs(dtc.attrs)
-        AssertGreat(len(list(self.model.attrs.values())),0)
+        #model.set_attrs(dtc.attrs)
+        assert len(list(self.model.attrs.values())) > 0
+        #self.AssertGreater(len(list(self.model.attrs.values())),0)
 
         rbt = get_neab.tests[0]
         scoreN = rbt.judge(model,stop_on_error = False, deep_error = True)
         import copy
         dtc.scores[str(rbt)] = copy.copy(scoreN.sort_key)
-        assertTrue(scoreN.sort_key is not None)
+        assert scoreN.sort_key is not None
+        self.assertTrue(scoreN.sort_key is not None)
         dtc.rheobase = copy.copy(scoreN.prediction)
         return dtc
 
-    def test_5rheobase_on_list(self):
+    def test_rheobase_on_list(self):
+        from neuronunit.optimization import exhaustive_search
         grid_points = self.grid_points
         second_point = grid_points[int(len(grid_points)/2)]
         three_points = [grid_points[0],second_point,grid_points[-1]]
-        assertEqual(len(three_points),3)
+        self.assertEqual(len(three_points),3)
         dtcpop = list(map(exhaustive_search.update_dtc_grid,three_points))
-        for i, dtc in enumerate(dtcpop):
-            dtcpop[i] = self.sub_test_backend(dtc)
+        for d in dtcpop:
+            print(type(d))
+            print(type(d.attrs))
+
+            import pdb; pdb.set_trace()
+
+            assert len(list(d.attrs.values())) > 0
+
+        #for i, dtc in enumerate(dtcpop):
+        dtcpop = self.test_01a_compute_score(dtcpop)
         self.dtcpop = dtcpop
         return dtcpop
 
-    def test_6neuron_set_attrs(self):
+    def test_neuron_set_attrs(self):
         from neuronunit.models.reduced import ReducedModel
         from neuronunit.optimization import get_neab
+        self.assertNotEqual(self.dtcpop,None)
+
         dtc = self.dtcpop[0]
+
         self.model = ReducedModel(get_neab.LEMS_MODEL_PATH, backend=('NEURON',{'DTC':dtc}))
-        AssertGreat(len(list(self.model.attrs.values())),0)
+        #self.AssertGreater(
+        assert len(list(self.model.attrs.values())) > 0
+        old_ = self.model.attrs.items()
+        #self.AssertGreater(len(list(self.model.attrs.values())),0)
+        self.model.set_attrs(dtc.attrs)
+
+        assert self.model.attrs.keys() in old_
+        assert self.model.attrs.values() in old_
+
+        #self.AssertGreater(len(list(self.model.attrs.values())),0)
+        assert len(list(self.model.attrs.values())) > 0
 
 
 
 
-    def test_1rheobase_serial(self):
+    def test_rheobase_serial(self):
         from neuronunit.optimization import data_transport_container
         from neuronunit.tests.fi import RheobaseTest as T
         score = self.run_test(T)
         self.rheobase = score.prediction
+        self.assertNotEqual(self.rheobase,None)
         #print(score)
 
 
-    def test_2inputresistance(self):
+    def test_inputresistance(self):
         from neuronunit.tests.passive import InputResistanceTest as T
         score = self.run_test(T)
         self.assertTrue(-0.6 < score < -0.5)
 
-    def test_3restingpotential(self):
+    def test_restingpotential(self):
         from neuronunit.tests.passive import RestingPotentialTest as T
         score = self.run_test(T)
         self.assertTrue(1.2 < score < 1.3)
 
-    def test_4capacitance(self):
+    def test_capacitance(self):
         from neuronunit.tests.passive import CapacitanceTest as T
         score = self.run_test(T)
         self.assertTrue(-0.15 < score < -0.05)
 
-    def test_5timeconstant(self):
+    def test_timeconstant(self):
         from neuronunit.tests.passive import TimeConstantTest as T
         score = self.run_test(T)
         self.assertTrue(-1.45 < score < -1.35)
 
 
-    def test_19ap_width(self):
+    def test_ap_width(self):
+        from neuronunit.models.reduced import ReducedModel
+        from neuronunit.optimization import get_neab
         from neuronunit.tests.waveform import InjectedCurrentAPWidthTest as T
         from neuronunit.optimization.optimization_management import format_test
         from neuronunit.optimization import data_transport_container
@@ -150,20 +211,25 @@ class TestBackend(NotebookTools,unittest.TestCase):
         score = self.run_test(T)
         self.assertTrue(-0.6 < score < -0.5)
 
-    def test_20ap_amplitude(self):
+    def test_ap_amplitude(self):
+        from neuronunit.models.reduced import ReducedModel
+        from neuronunit.optimization import get_neab
         from neuronunit.tests.waveform import InjectedCurrentAPAmplitudeTest as T
         from neuronunit.optimization.optimization_management import format_test
         from neuronunit.optimization import data_transport_container
         dtc = data_transport_container.DataTC()
         dtc.rheobase = self.rheobase
+        #def run_test(self, cls, pred =None):
         dtc = format_test(dtc)
         self.model = ReducedModel(get_neab.LEMS_MODEL_PATH, backend=('NEURON',{'DTC':dtc}))
 
         #self.update_amplitude(T)
-        score = self.run_test(T)
+        score = self.run_test(T,pred = dtc.rheobase)
         self.assertTrue(-1.7 < score < -1.6)
 
-    def test_21ap_threshold(self):
+    def test_ap_threshold(self):
+        from neuronunit.models.reduced import ReducedModel
+        from neuronunit.optimization import get_neab
         from neuronunit.tests.waveform import InjectedCurrentAPThresholdTest as T
         from neuronunit.optimization.optimization_management import format_test
         from neuronunit.optimization import data_transport_container
@@ -176,7 +242,7 @@ class TestBackend(NotebookTools,unittest.TestCase):
 
 
 
-    def test_1rheobase_single_value_parallel_and_serial_comparison(self):
+    def test_rheobase_single_value_parallel_and_serial_comparison(self):
         from neuronunit.tests.fi import RheobaseTest, RheobaseTestP
         from neuronunit.optimization import get_neab
         from neuronunit.models.reduced import ReducedModel
---
+++ neuronunit/unit_test/test_backends.py
@@ -0,0 +1,116 @@
+from neuronunit.tests.fi import RheobaseTest, RheobaseTestP
+#from neuronunit.optimization.get_neab import fi_basket_tests
+from neuronunit.models.reduced import ReducedModel
+from neuronunit import aibs
+from neuronunit.models.reduced import ReducedModel
+from neuronunit.optimization import get_neab
+from neuronunit.optimization.model_parameters import model_params
+from neuronunit.optimization import exhaustive_search
+from neuronunit.optimization.optimization_management import dtc_to_rheo
+from neuronunit.optimization.optimization_management import nunit_evaluation
+from neuronunit.optimization.optimization_management import format_test
+from neuronunit.optimization.exhaustive_search import update_dtc_grid
+
+import os
+import dask.bag as db
+
+from neuronunit.optimization import get_neab
+from neuronunit.models.reduced import ReducedModel
+
+
+npoints = 2
+nparams = 10
+provided_keys = list(model_params.keys())
+USE_CACHED_GS = False
+grid_points = exhaustive_search.create_grid(npoints = npoints,nparams = nparams)
+#dtcpop = list(update_dtc_pop(grid_points,td = td))
+b0 = db.from_sequence(grid_points, npartitions=8)
+dtcpop = list(db.map(update_dtc_grid,b0).compute())
+print(dtcpop)
+N = 3
+
+
+def sub_test_backend(dtc):
+    import copy
+    import unittest
+
+    dtc = copy.copy(dtc)
+    dtc.scores = {}
+    #unittest.TestCase
+    #from unittest.TestCase import assertGreater
+    size = len(list(dtc.attrs.values()))
+    unittest.case.TestCase.assertGreater(unittest.case.TestCase,size,0)
+    model = ReducedModel(get_neab.LEMS_MODEL_PATH, name= str('vanilla'), backend=('NEURON', {'DTC':dtc}))
+    unittest.case.TestCase.assertNotEqual(unittest.case.TestCase,model.attrs,None)
+
+    rbt = get_neab.tests[0]
+    scoreN = rbt.judge(model,stop_on_error = False, deep_error = True)
+    import copy
+    dtc.scores[str(rbt)] = copy.copy(scoreN.sort_key)
+    dtc.rheobase = copy.copy(scoreN.prediction)
+    return dtc
+
+
+
+def test_backend(grid_points):
+    second_point = grid_points[int(len(grid_points)/2)]
+    dtcpop = list(map(exhaustive_search.update_dtc_grid,[grid_points[0],second_point,grid_points[-1]]))
+    for i, dtc in enumerate(dtcpop):
+        dtcpop[i] = sub_test_backend(dtc)
+    return dtcpop
+
+def test_serial(grid_points):
+    models = test_backend(grid_points)
+    models = list(map(format_test,models))#.compute())
+    models = list(map(nunit_evaluation,models))#.compute())
+    #self.modelss = models
+    return models
+
+def map_wrapper(function_item,list_items):
+    b0 = db.from_sequence(list_items, npartitions=8)
+    list_items = list(db.map(function_item,b0).compute())
+    return list_items
+
+def test_parallel(grid_points):
+    models = test_backend(grid_points)
+    b0 = db.from_sequence(models, npartitions=8)
+    models = list(db.map(format_test,b0).compute())
+    b0 = db.from_sequence(models, npartitions=8)
+    models = list(db.map(nunit_evaluation,b0).compute())
+    #self.modelsp = models
+    return models
+
+
+def serial_equals_parallel():
+    import time
+    start_serial = time.time()
+    dtcs = test_serial(grid_points)
+    end_serial = time.time()
+    serial_length = end_serial - start_serial
+    start_parallel = time.time()
+    dtcp = test_parallel(grid_points)
+    end_parallel = time.time()
+    parallel_length = end_parallel - start_parallel
+    #print('note these time disparities take for granted benefit of parallel rheobase in both instances')
+    #print('serial', serial_length, 'parallel length', parallel_length)
+    #print('serial', serial_length, 'parallel length', parallel_length)
+    return dtcp, dtcs
+
+#dtcp, dtcs = serial_equals_parallel()
+import unittest
+
+class TestBackend(unittest.TestCase):#,serial_equals_parallel):
+    def setUp(self):
+        dtcp, dtcs = serial_equals_parallel()
+        self.dtcp = dtcp # 5
+        self.dtcs = dtcs # 4
+
+    def test(self):
+        dtcp, dtcs = serial_equals_parallel()
+
+        for i,d0 in enumerate(dtcp):
+            for k, v in d0.scores.items():
+                print('serial scores equal parallel scores')
+                assert dtcs[i].scores[k] == v
+                self.assertEquals(dtcs[i].scores[k],v)
+        return 0
