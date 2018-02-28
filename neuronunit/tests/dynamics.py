77cae69d1b16a72605b29ba27526ab24e1375d43
Russell Jarvis <rjjarvis@asu.edu>
2018-02-27 20:53:04 -0700

    added in ISITest and firing rate tests

--- neuronunit/tests/dynamics.py
+++ neuronunit/tests/dynamics.py
@@ -156,40 +156,16 @@ class FiringRateTest(RheobaseTest):
              "type 1 or type 2 threshold firing rate dynamics.")
     '''
 
-    def generate_prediction(self, model = None, waveform =None):
+    def generate_prediction(self, model = None):
         """Implementation of sciunit.Test.generate_prediction."""
 
-        if waveform is not None:
-            prediction = lv(waveform)
-        return prediction
+        ass = model.get_membrane_potential()
+        spike_count = model.get_spike_count()
 
-        '''
-        model.rerun = True
-        if 'rheobase' in self.observation:
-            guess = self.observation['rheobase']
-        else:
-            guess = 100.0*pq.pA
-        lookup = self.threshold_FI(model, self.units, guess=guess)
-        sub = np.array([x for x in lookup if lookup[x]==0])*self.units
-        supra = np.array([x for x in lookup if lookup[x]>0])*self.units
-        if self.verbose:
-            if len(sub):
-                print("Highest subthreshold current is %s" \
-                      % float(sub.max().round(2)))
-            else:
-                print("No subthreshold current was tested.")
-            if len(supra):
-                print("Lowest suprathreshold current is %s" \
-                      % supra.min().round(2))
-            else:
-                print("No suprathreshold current was tested.")
-
-        prediction = None
-        if len(sub) and len(supra):
-            sub = np.array([x for x in lookup if lookup[x]<0]) # No units
-            below_thresh_i = sub.max()
-            _ = self.threshold_FI(model, self.units, guess = below_thresh_i)
-        '''
+        window = ass.t_stop-ass.t_start
+        predicted_rate = float(spike_count/window)
+        # prediction should be number of spikes divided by time.
+        return predicted_rate
 
 
 
@@ -200,7 +176,58 @@ class FiringRateTest(RheobaseTest):
         if prediction is None:
             score = scores.InsufficientDataScore(None)
         else:
-            score = self.score_type.compute(observation,prediction,key='cv')
+            score = self.score_type.compute(observation,prediction,key='f_rate')
+        return score
+
+
+class ISITest(VmTest):
+    """Tests whether a model exhibits the observed burstiness"""
+
+    def __init__(self, observation={'isi_mean':None,'isi_std':None},
+                 name=None,
+                 params=None):
+        pass
+
+    name = "Local Variation test"
+    description = (("For neurons and muscle cells with slower non firing dynamics like CElegans neurons check to see how much \
+    varition is in the continuous membrane potential."))
+    score_type = scores.RatioScore
+    units = pq.ms
+    #local_variation = 0.0 # 1.0
+
+    def __init__(self, *args, **kwargs):
+        super(ISITest,self).__init__(*args,**kwargs)
+        #super(TFRTypeTest,self).__init__(*args,**kwargs)
+
+        if self.name == self.__class__.name:
+            self.name = "Firing Rate Type %d test" % self.observation['type']
+
+    '''
+    def validate_observation(self, observation):
+        super(CVTest,self).validate_observation(observation,
+                                                     united_keys=['rheobase'],
+                                                     nonunited_keys=['type'])
+
+        assert ('type' in observation) and (observation['type'] in [1,2]), \
+            ("observation['type'] must be either 1 or 2, corresponding to "
+             "type 1 or type 2 threshold firing rate dynamics.")
+    '''
+
+    def generate_prediction(self, model = None):
+        prediction = np.mean(isi(model.get_membrane_potential()))
+        return prediction
+
+
+
+
+    def compute_score(self, observation, prediction):
+        """Implementation of sciunit.Test.score_prediction."""
+        #print("%s: Observation = %s, Prediction = %s" % \
+        #    (self.name,str(observation),str(prediction)))
+        if prediction is None:
+            score = scores.InsufficientDataScore(None)
+        else:
+            score = self.score_type.compute(observation,prediction,key='isi')
         return score
 
 
@@ -261,6 +288,9 @@ class LocalVariationTest(VmTest):
                  name=None,
                  params=None):
         pass
+
+    required_capabilities = (cap.ReceivesSquareCurrent,
+                             cap.ProducesSpikes)
 
     name = "Local Variation test"
     description = (("For neurons and muscle cells with slower non firing dynamics like CElegans neurons check to see how much \
