79f40ab9100de8d641d794363b7b1c47378c9f3d
Richard C Gerkin <rick1@gerkin.org>
2018-03-02 13:31:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 try:
     from setuptools import setup
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
 
 setup(
     name='neuronunit',
     version='0.19',
     author='Rick Gerkin',
     author_email='rgerkin@asu.edu',
-        packages=[
-            'neuronunit',
-            'neuronunit.capabilities',
-            'neuronunit.neuroconstruct',
-            'neuronunit.models',
-            'neuronunit.tests',
-            'neuronunit.optimization',
-            'neuronunit.unit_test'],
+    packages=find_packages(),
     url='http://github.com/scidash/neuronunit',
     license='MIT',
     description='A SciUnit library for data-driven testing of single-neuron physiology models.',
