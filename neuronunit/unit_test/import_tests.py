d6152b19604ed3e70305c627eb3b134ffadbb276
Richard C Gerkin <rgerkin@asu.edu>
2018-03-06 16:42:49 -0700

    Skip import test for modules that are deprecated or have high install complexity

--- neuronunit/unit_test/import_tests.py
+++ neuronunit/unit_test/import_tests.py
@@ -10,7 +10,10 @@ class ImportTestCase(unittest.TestCase):
     def test_import_everything(self):
         import neuronunit
         # Recursively import all submodules
-        import_all_modules(neuronunit)
+        import_all_modules(neuronunit,
+                           skip=['neuroconstruct','optimization',
+                                 'backends','unit_test'],
+                           verbose=True)
 
 
 if __name__ == '__main__':
