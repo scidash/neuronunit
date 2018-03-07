7796f0fbc02bfef150fa4e2048c5082f99c1c7e9
Richard C Gerkin <rgerkin@asu.edu>
2018-03-06 17:15:30 -0700

    Using sciunit unit test directory structure
    Former-commit-id: d5fdc3d06e765304979e8a2f81ddabae947c2ef8

--- neuronunit/unit_test/__init__.py
+++ neuronunit/unit_test/__init__.py
@@ -1,1 +1,14 @@
-"""Unit tests for NeuronUnit"""
+"""Unit testing module for NeuronUnit"""
+
+import unittest
+
+from .base import *
+from .import_tests import ImportTestCase
+from .doc_tests import DocumentationTestCase
+from .resource_tests import NeuroElectroTestCase,BlueBrainTestCase,AIBSTestCase
+from .model_tests import ReducedModelTestCase
+from .test_tests import TestsPassiveTestCase,TestsWaveformTestCase,\
+                        TestsFITestCase,TestsDynamicsTestCase,\
+                        TestsChannelTestCase
+from .misc_tests import EphysPropertiesTestCase
+from .sciunit_tests import SciUnitTestCase
