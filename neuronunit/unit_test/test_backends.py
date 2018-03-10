ecba5393ccdd6992d51ab714dd5527cd8c8ef6ce
Russell Jarvis <rjjarvis@asu.edu>
2018-03-10 16:49:05 -0700

    clean up the single test

--- neuronunit/unit_test/test_backends.py
+++ neuronunit/unit_test/test_backends.py
@@ -1,5 +1,4 @@
 from neuronunit.tests.fi import RheobaseTest, RheobaseTestP
-#from neuronunit.optimization.get_neab import fi_basket_tests
 from neuronunit.models.reduced import ReducedModel
 from neuronunit import aibs
 from neuronunit.models.reduced import ReducedModel
@@ -10,14 +9,10 @@ from neuronunit.optimization.optimization_management import dtc_to_rheo
 from neuronunit.optimization.optimization_management import nunit_evaluation
 from neuronunit.optimization.optimization_management import format_test
 from neuronunit.optimization.exhaustive_search import update_dtc_grid
-
 import os
 import dask.bag as db
-
 from neuronunit.optimization import get_neab
 from neuronunit.models.reduced import ReducedModel
-
-
 npoints = 2
 nparams = 10
 provided_keys = list(model_params.keys())
@@ -32,16 +27,12 @@ N = 3
 def sub_test_backend(dtc):
     import copy
     import unittest
-
     dtc = copy.copy(dtc)
     dtc.scores = {}
-    #unittest.TestCase
-    #from unittest.TestCase import assertGreater
     size = len(list(dtc.attrs.values()))
     unittest.case.TestCase.assertGreater(unittest.case.TestCase,size,0)
     model = ReducedModel(get_neab.LEMS_MODEL_PATH, name= str('vanilla'), backend=('NEURON', {'DTC':dtc}))
     unittest.case.TestCase.assertNotEqual(unittest.case.TestCase,model.attrs,None)
-
     rbt = get_neab.tests[0]
     scoreN = rbt.judge(model,stop_on_error = False, deep_error = True)
     import copy
@@ -60,9 +51,8 @@ def test_backend(grid_points):
 
 def test_serial(grid_points):
     models = test_backend(grid_points)
-    models = list(map(format_test,models))#.compute())
-    models = list(map(nunit_evaluation,models))#.compute())
-    #self.modelss = models
+    models = list(map(format_test,models))
+    models = list(map(nunit_evaluation,models))
     return models
 
 def map_wrapper(function_item,list_items):
@@ -76,7 +66,6 @@ def test_parallel(grid_points):
     models = list(db.map(format_test,b0).compute())
     b0 = db.from_sequence(models, npartitions=8)
     models = list(db.map(nunit_evaluation,b0).compute())
-    #self.modelsp = models
     return models
 
 
@@ -96,23 +85,17 @@ def serial_equals_parallel():
     return dtcp, dtcs
 
 import unittest
-
 class TestBackend(unittest.TestCase):#,serial_equals_parallel):
     def setUp(self):
-        #dtcp, dtcs = serial_equals_parallel()
-        #self.dtcp = dtcp # 5
-        #self.dtcs = dtcs # 4
-
+        pass
     def test(self):
         dtcp, dtcs = serial_equals_parallel()
-
         for i,d0 in enumerate(dtcp):
             for k, v in d0.scores.items():
-                print('serial scores equal parallel scores')
-
+                print('serial scores equal parallel scores:')
                 self.assertEqual(dtcs[i].scores[k],v)
                 self.assertEquals(dtcs[i].scores[k],v)
-        return True        
+        return True
 
 import sys
 sys.exit()
