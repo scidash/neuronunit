dddb988af08ebf70ba6968b0495efbb34a073916
Russell Jarvis <rjjarvis@asu.edu>
2018-05-16 10:29:49 -0700

    created a neuro electro query for 3 new classes of cells, 4 cells total

--- neuronunit/unit_test/pipe_entry_point.py
+++ neuronunit/unit_test/pipe_entry_point.py
@@ -1,15 +1,13 @@
 from neuronunit.optimization import get_neab #import get_neuron_criteria, impute_criteria
 import os
 import pickle
+from neuronunit.optimization.model_parameters import model_params
+from bluepyopt.deapext.optimisations import DEAPOptimisation
+from neuronunit.tests.fi import RheobaseTestP
+from dask import distributed
+import pickle
+
 electro_path = 'pipe_tests.p'
-#try:
-assert os.path.isfile(electro_path) == True
-with open(electro_path,'rb') as f:
-    electro_tests = pickle.load(f)
-
-#except:
-print('hello')
-
 fi_basket = {'nlex_id':'NLXCELL:100201'}
 # https://scicrunch.org/scicrunch/interlex/view/ilx_0107386
 pvis_cortex = {'nlex_id': 'nifext_50'} # Layer V pyramidal cell
@@ -23,42 +21,94 @@ olf_mitral = { 'nlex_id':'NLXWIKI:nifext_120'}
 #https://scicrunch.org/scicrunch/interlex/view/ilx_0107386
 ca1_pyr = { 'nlex_id':'SAO:830368389'}
 pipe = [ fi_basket, pvis_cortex, ca1_pyr, purkinje, ca1_pyr ]
-import pickle
-with open('test_pipe.p','wb') as handle:
-    pickle.dump(pipe,handle)
-contents = pickle.load(open('ne_neuron_criteria.p','rb'))
-pvis_criterion, inh_criterion = contents
-electro_tests = []
-contents[0][0].observation
-for p in pipe:
-   p_tests, p_observations = get_neab.get_neuron_criteria(p)
-   electro_tests.append((p_tests, p_observations))
-with open('pipe_tests.p','wb') as f:
-   pickle.dump(electro_tests,f)
+
+def replace_zero_std(electro_tests):
+    for test,obs in electro_tests:
+        test[0] = RheobaseTestP(obs['Rheobase'])
+        for k,v in obs.items():
+            if v['std'] == 0:
+                obs = get_neab.substitute_criteria(electro_tests[1][1],obs)
+    return electro_tests
+
+try:
+    assert os.path.isfile(electro_path) == True
+    with open(electro_path,'rb') as f:
+        electro_tests = pickle.load(f)
+    electro_tests = replace_zero_std(electro_tests)
+except:
+
+    electro_tests = []
+    contents[0][0].observation
+    for p in pipe:
+       p_tests, p_observations = get_neab.get_neuron_criteria(p)
+       electro_tests.append((p_tests, p_observations))
+    '''
+    Add in the parallel rheobase test.
+    '''
+    electro_tests = replace_zero_std(electro_tests)
+    with open('pipe_tests.p','wb') as f:
+       pickle.dump(electro_tests,f)
 
 MU = 6; NGEN = 6; CXPB = 0.9
+
 USE_CACHED_GA = False
-print(get_neab)
-from neuronunit.optimization.model_parameters import model_params
 provided_keys = list(model_params.keys())
-USE_CACHED_GS = False
-from bluepyopt.deapext.optimisations import DEAPOptimisation
+
 npoints = 2
 nparams = 10
 
-from dask import distributed
-#c = distributed.Client()
-#NCORES = len(c.ncores().values())-2
-for test, observation in electro_tests:
-    DO = DEAPOptimisation(error_criterion = test, selection = 'selIBEA')
-    DO.setnparams(nparams = nparams, provided_keys = provided_keys)
-    pop, hof_py, log, history, td_py, gen_vs_hof = DO.run(offspring_size = MU, max_ngen = NGEN, cp_frequency=0,cp_filename='ga_dumpnifext_50.p')
+import numpy as np
 
-#with open('ga_dump_NLXCELL:100201.p','wb') as f:
-#   pickle.dump([pop, log, history, hof_py, td_py],f)
-from neuronunit.tests.fi import RheobaseTestP
-for test,obs in electro_tests:
-    test[0] = RheobaseTestP(obs)
+cnt = 0
+pipe_results = {}
+import pandas as pd
+
+def check_dif(pipe_old,pipe_new):
+    bool = False
+    for key, value in pipe_results.items():
+        if value != pipe_new[key]:
+            bool = True
+        print(value,pipe_new[key])
+    '''
+    for index, val in enumerate(pipe_results.values()):
+        if index == 0:
+            attrs = pd.DataFrame(list(val['pop'][0].dtc.attrs.values())).T
+        else:
+            attrs = attrs.append(pd.DataFrame(list(val['pop'][0].dtc.attrs.values())).T)
+    attrs.columns = val['pop'][0].dtc.attrs.keys()
+    '''
+    return bool
+
+import pdb; pdb.set_trace()
+
+for test, observation in electro_tests:
+    dic_key = str(list(pipe[cnt].values())[0])
+    DO = None
+    DO = DEAPOptimisation(error_criterion = test, selection = 'selIBEA', provided_dict = model_params)
+    pop, hof_py, log, history, td_py, gen_vs_hof = DO.run(offspring_size = MU, max_ngen = NGEN, cp_frequency=1,cp_filename=str(dic_key)+'.p')
+    import pdb; pdb.set_trace()
+
+    #with open(str(dic_key)+'.p','rb') as f:
+
+    #    check_point = pickle.load(f)
+    pipe_results[dic_key] = {}
+    pipe_results[dic_key]['pop'] = pop
+    pipe_results[dic_key]['hof_py'] = hof_py
+    pipe_results[dic_key]['log'] = log
+    pipe_results[dic_key]['history'] = history
+    pipe_results[dic_key]['td_py'] = td_py
+    pipe_results[dic_key]['gen_vs_hof'] = gen_vs_hof
+    if cnt > 1:
+        import pdb; pdb.set_trace()
+        assert check_dif(pipe_old,pipe_results) == True
+        assert np.mean(pipe_results[dic_key]['gen_vs_hof']) != np.mean(pipe_results[previous]['gen_vs_hof'])
+        assert np.mean(pipe_results[dic_key]['history']) != np.mean(pipe_results[previous]['history'])
+        pipe_old = pipe_results[dic_key]#['pop'][0].attrs
+    previous = dic_key
+    cnt += 1
+
+with open('dump_all_cells','wb') as f:
+   pickle.dump(pipe_results,f)
 
 
 
