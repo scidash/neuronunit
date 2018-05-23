bc9edd92ae6fefe06fbd1878778c9f549461c88c
Russell Jarvis <colouredstatic@gmail.com>
2018-05-23 15:59:24 -0700

    Update post_install.sh

--- neuronunit/unit_test/post_install.sh
+++ neuronunit/unit_test/post_install.sh
@@ -2,6 +2,3 @@ cd $HOME
 sudo /opt/conda/bin/pip install -e BluePyOpt
 sudo /opt/conda/bin/pip install -e neuronunit
 sudo /opt/conda/bin/pip install git+https://github.com/NeuroML/pyNeuroML
-cd $HOME
-sudo rm -r $HOME/scoop
-sudo rm -r $HOME/work
