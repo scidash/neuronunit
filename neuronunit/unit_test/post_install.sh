f83032086baaa3ce2de4bffb81aeedf0c1d41763
Russell Jarvis <rjjarvis@asu.edu>
2018-05-23 15:50:47 -0700

    bash script to supplament Dockerbuild

---
+++ neuronunit/unit_test/post_install.sh
@@ -0,0 +1,7 @@
+cd $HOME
+sudo /opt/conda/bin/pip install -e BluePyOpt
+sudo /opt/conda/bin/pip install -e neuronunit
+sudo /opt/conda/bin/pip install git+https://github.com/NeuroML/pyNeuroML
+cd $HOME
+sudo rm -r $HOME/scoop
+sudo rm -r $HOME/work
