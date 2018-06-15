59446153906f38e4788cb521b76c740630f89d98
Russell Jarvis <rjjarvis@asu.edu>
2018-06-15 10:58:03 -0700

    Revert "Added unit tests back"
    This reverts commit a0a38ed9e276eac09875b8542f96c8f9ed6d1152 [formerly ee801e1f62ffbdece6b40d31394ac00ab30473b7].
    
    
    Former-commit-id: ebce97241399dfb078b78d736cc498416caa82d4

--- neuronunit/unit_test/__init__.py
+++ neuronunit/unit_test/__init__.py
@@ -1,16 +1,5 @@
-"""Unit testing module for NeuronUnit"""
- 
-import unittest
- 
-from .base import *
-from .import_tests import ImportTestCase
-from .doc_tests import DocumentationTestCase
-from .resource_tests import NeuroElectroTestCase,BlueBrainTestCase,AIBSTestCase
-from .model_tests import ReducedModelTestCase
-from .test_tests import TestsPassiveTestCase,TestsWaveformTestCase,\
-                        TestsFITestCase,TestsDynamicsTestCase,\
-                        TestsChannelTestCase
-from .misc_tests import EphysPropertiesTestCase
-from .sciunit_tests import SciUnitTestCase
+
+"""NeuronUnit Optimization classes"""
+
 #from .evaluate_as_module import *
 #from .nsga_parallel import *
