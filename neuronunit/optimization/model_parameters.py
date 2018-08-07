bb0bc5446b245e130333e05dbe5d3b6bb43e811a
Russell Jarvis <colouredstatic@gmail.com>
2018-08-07 19:24:07 -0400

    changes for spike server

--- neuronunit/optimization/model_parameters.py
+++ neuronunit/optimization/model_parameters.py
@@ -9,19 +9,68 @@ path_params['model_path'] = os.path.realpath(os.path.join(THIS_DIR,'..','models'
 # Which Parameters
 # https://www.izhikevich.org/publications/spikes.htm
 
-model_params = OrderedDict()
-model_params['vr'] = np.linspace(-100.0,-40.0,9)
-model_params['a'] = np.linspace(0.0,0.09,9)
-model_params['b'] = np.linspace(0.0,0.99,9)
-model_params['vpeak'] =np.linspace(30.0,40.0,9)
-model_params['k'] = np.linspace(7.0E-4-+7.0E-5,7.0E-4+70E-5,9)
-model_params['C'] = np.linspace(1.00000005E-4-1.00000005E-5,1.00000005E-4+1.00000005E-5,9)
-model_params['c'] = np.linspace(-80,-45,9)
-model_params['d'] = np.linspace(0.050,10.0,9)
-model_params['v0'] = np.linspace(-75.0,-45.0,9)
-model_params['vt'] =  np.linspace(-50.0,-30.0,9)
 
+
+
+#import collections
+#from neuron import h
+#dummy = h.Section()
+#
+import collections
+import numpy as np
+
+type2007 = collections.OrderedDict([
+  #              C    k     vr  vt vpeak   a      b   c    d  celltype
+  ('RS',        (100, 0.7,  -60, -40, 35, 0.03,   -2, -50,  100,  1)),
+  ('IB',        (150, 1.2,  -75, -45, 50, 0.01,   5, -56,  130,   2)),
+  ('CH',        (50,  1.5,  -60, -40, 25, 0.03,   1, -40,  150,   3)),
+  ('LTS',       (100, 1.0,  -56, -42, 40, 0.03,   8, -53,   20,   4)),
+  ('FS',        (20,  1.0,  -55, -40, 25, 0.2,   -2, -45,  -55,   5)),
+  ('TC',        (200, 1.6,  -60, -50, 35, 0.01,  15, -60,   10,   6)),
+  ('TC_burst',  (200, 1.6,  -60, -50, 35, 0.01,  15, -60,   10,   6)),
+  ('RTN',       (40,  0.25, -65, -45,  0, 0.015, 10, -55,   50,   7)),
+  ('RTN_burst', (40,  0.25, -65, -45,  0, 0.015, 10, -55,   50,   7))])
+
+
+
+temp = {k:[] for k in ['C','k','vr','vt','vpeak','a','b','c','d']  }
+for i,k in enumerate(temp.keys()):
+    for v in type2007.values():
+        temp[k].append(v[i])
+
+explore_param = {k:(np.min(v),np.max(v)) for k,v in temp.items()}
+model_params = OrderedDict(explore_param)
+
+
+remap = ['C','k','a','b','d' ]
+for k in model_params.keys():
+    if k in remap:
+        model_params[k] = np.linspace(float(model_params[k][0])*(10**-4),float(model_params[k][1])*(10**-4),9)
+    else:
+        model_params[k] = np.linspace(float(model_params[k][0]),float(model_params[k][1]),9)
+
+print(model_params)
 '''
+
+https://www.izhikevich.org/publications/izhikevich.m
+
+Model parameters and dimensions:
+t                      time     [ms]
+C                    membrane capacitance     [pF = pA.ms.mV-1]
+v                     membrane potential     [mV]
+         rate of change of membrane potential     [mV.ms-1 = V.s-1]
+       capacitor current     [pA]
+vr                   resting membrane potential     [mV]
+vt                   instantaneous threshold potential     [mV]
+k                    constant (“1/R”)     [pA.mV-1   (10-9 Ω-1)]
+u                   recovery variable     [pA]
+S                   stimulus (synaptic: excitatory or inhibitory, external, noise)     [pA]
+       rate of change of recovery variable     [pA.ms-1]
+a                     recovery time constant     [ms-1]
+b                  constant  (“1/R”)    [pA.mV-1   (10-9 Ω-1) ]
+c                  potential reset value     [mV]
+d                 outward minus inward currents activated during the spike and affecting the after-spike behavior     [pA]
+vpeak          spike cutoff value     [mV]
 +model_params['vr'] = np.linspace(-95.0,-30.0,9)
 
 +model_params['a'] = np.linspace(0.0,0.945,9)
