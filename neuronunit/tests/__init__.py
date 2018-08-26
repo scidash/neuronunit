0554ca9d6e62697245936f98040094f9361c65a2
Russell Jarvis <colouredstatic@gmail.com>
2018-08-25 17:06:07 -0700

    dev changes

--- neuronunit/tests/__init__.py
+++ neuronunit/tests/__init__.py
@@ -5,3 +5,27 @@ from .passive import *
 from .waveform import *
 from .dynamics import *
 from .fi import *
+
+from sciunit import scores, errors
+
+from sciunit.errors import CapabilityError, InvalidScoreError
+
+class FakeTest(sciunit.Test):
+
+    #from sciunit.errors import CapabilityError, InvalidScoreError
+
+    #score_type = scores.RatioScore
+    score_type = sciunit.scores.ZScore
+
+    def generate_prediction(self, model):
+        self.key_param = self.name.split('_')[1]
+        self.prediction = model.attrs[self.key_param]
+        return self.prediction
+
+    def compute_score(self, observation, prediction):
+        mean = observation[0]
+        std = observation[1]
+        z = (prediction - mean)/std
+        #self.prediction = prediction
+        #print(scores.ZScore(z))
+        return scores.ZScore(z)
