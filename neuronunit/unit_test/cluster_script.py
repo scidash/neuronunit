a8469ef3ca5481db0b193583597825bf827ba5a4
Russell Jarvis <colouredstatic@gmail.com>
2019-01-12 11:59:42 -0700

    script to run on cluster
    Former-commit-id: 6a2babc59d69399c0286c707babe1aa05b3b3050

---
+++ neuronunit/unit_test/cluster_script.py
@@ -0,0 +1,200 @@
+
+# coding: utf-8
+
+# Assumptions, the environment for running this notebook was arrived at by building a dedicated docker file.
+#
+# https://cloud.docker.com/repository/registry-1.docker.io/russelljarvis/nuo
+#
+# You can run use dockerhub to get the appropriate file, and launch this notebook using Kitematic.
+
+# In[ ]:
+
+# This is code, change cell type to markdown.
+# ![alt text](plan.jpg "Pub plan")
+
+
+# # Import libraries
+# To keep the standard running version of minimal and memory efficient, not all available packages are loaded by default. In the cell below I import a mixture common python modules, and custom developed modules associated with NeuronUnit (NU) development
+
+# In[ ]:
+
+#!pip install dask distributed seaborn
+#!bash after_install.sh
+import numpy as np
+import os
+import pickle
+import pandas as pd
+from neuronunit.tests.fi import RheobaseTestP
+from neuronunit.optimization.model_parameters import reduced_dict, reduced_cells
+from neuronunit.optimization import optimization_management as om
+from sciunit import scores# score_type
+
+from neuronunit.optimization.data_transport_container import DataTC
+from neuronunit.tests.fi import RheobaseTestP# as discovery
+from neuronunit.optimization.optimization_management import dtc_to_rheo, format_test, nunit_evaluation
+import quantities as pq
+from neuronunit.models.reduced import ReducedModel
+from neuronunit.optimization.model_parameters import model_params, path_params
+LEMS_MODEL_PATH = path_params['model_path']
+list_to_frame = []
+from neuronunit.tests.fi import RheobaseTestP
+
+
+# from IPython.display import HTML, display
+# import seaborn as sns
+
+
+
+
+
+# # The Izhiketich model is instanced using some well researched parameter sets.
+#
+
+# First lets get the points in parameter space, that Izhikich himself has published about. These points are often used by the open source brain project to establish between model reproducibility. The itial motivating factor for choosing these points as constellations, of all possible parameter space subsets, is that these points where initially tuned and used as best guesses for matching real observed experimental recordings.
+
+# In[ ]:
+
+explore_param = {k:(np.min(v),np.max(v)) for k,v in reduced_dict.items()}
+
+
+# ## Get the experimental Data pertaining to four different classes or neurons, that can constrain models.
+# Next we get some electro physiology data for four different classes of cells that are very common targets of scientific neuronal modelling. We are interested in finding out what are the most minimal, and detail reduced, low complexity model equations, that are able to satisfy
+
+# Below are some of the data set ID's I used to query neuroelectro.
+# To save time for the reader, I prepared some data earlier to save time, and saved the data as a pickle, pythons preferred serialisation format.
+#
+# The interested reader can find some methods for getting cell specific ephys data from neuroelectro in a code file (neuronunit/optimization/get_neab.py)
+#
+
+# In[ ]:
+
+purkinje ={"id": 18, "name": "Cerebellum Purkinje cell", "neuron_db_id": 271, "nlex_id": "sao471801888"}
+fi_basket = {"id": 65, "name": "Dentate gyrus basket cell", "neuron_db_id": None, "nlex_id": "nlx_cell_100201"}
+pvis_cortex = {"id": 111, "name": "Neocortex pyramidal cell layer 5-6", "neuron_db_id": 265, "nlex_id": "nifext_50"}
+#does not have rheobase
+olf_mitral = {"id": 129, "name": "Olfactory bulb (main) mitral cell", "neuron_db_id": 267, "nlex_id": "nlx_anat_100201"}
+ca1_pyr = {"id": 85, "name": "Hippocampus CA1 pyramidal cell", "neuron_db_id": 258, "nlex_id": "sao830368389"}
+pipe = [ fi_basket, ca1_pyr, purkinje,  pvis_cortex]
+
+
+# In[ ]:
+
+electro_tests = []
+obs_frame = {}
+test_frame = {}
+
+try:
+
+    electro_path = str(os.getcwd())+'all_tests.p'
+
+    assert os.path.isfile(electro_path) == True
+    with open(electro_path,'rb') as f:
+        (obs_frame,test_frame) = pickle.load(f)
+
+except:
+    for p in pipe:
+        p_tests, p_observations = get_neab.get_neuron_criteria(p)
+        obs_frame[p["name"]] = p_observations#, p_tests))
+        test_frame[p["name"]] = p_tests#, p_tests))
+    electro_path = str(os.getcwd())+'all_tests.p'
+    with open(electro_path,'wb') as f:
+        pickle.dump((obs_frame,test_frame),f)
+
+
+# # Cast the tabulatable data to pandas data frame
+# There are many among us who prefer potentially tabulatable data to be encoded in pandas data frame.
+
+for k,v in test_frame.items():
+    if "olf_mit" not in k:
+        obs = obs_frame[k]
+        v[0] = RheobaseTestP(obs['Rheobase'])
+df = pd.DataFrame.from_dict(obs_frame)
+print(test_frame.keys())
+
+
+# In the data frame below, you can see many different cell types
+
+df['Hippocampus CA1 pyramidal cell']
+
+
+
+# # Tweak Izhikitich equations
+# with educated guesses based on information that is already encoded in the predefined experimental observations.
+#
+# In otherwords use information that is readily amenable into hardcoding into equations
+#
+# Select out the 'Neocortex pyramidal cell layer 5-6' below, as a target for optimization
+
+
+free_params = ['a','b','k','c','C','d','vPeak','vr']
+hc_ = reduced_cells['RS']
+hc_['vr'] = -65.2261863636364
+hc_['vPeak'] = hc_['vr'] + 86.364525297619
+explore_param['C'] = (hc_['C']-20,hc_['C']+20)
+explore_param['vr'] = (hc_['vr']-5,hc_['vr']+5)
+use_test = test_frame["Neocortex pyramidal cell layer 5-6"]
+
+#for t in use_test[::-1]:
+#    t.score_type = scores.RatioScore
+test_opt = {}
+
+with open('data_dump.p','wb') as f:
+    pickle.dump(test_opt,f)
+
+
+# In[ ]:
+
+
+use_test[0].observation
+print(use_test[0].name)
+
+rtp = RheobaseTestP(use_test[0].observation)
+use_test[0] = rtp
+print(use_test[0].observation)
+
+
+reduced_cells.keys()
+test_frame.keys()
+test_frame.keys()
+test_frame['olf_mit'].insert(0,test_frame['Cerebellum Purkinje cell'][0])
+test_frame
+
+
+
+
+df = pd.DataFrame(index=list(test_frame.keys()),columns=list(reduced_cells.keys()))
+
+for k,v in reduced_cells.items():
+    temp = {}
+    temp[str(v)] = {}
+    dtc = DataTC()
+    dtc.tests = use_test
+    dtc.attrs = v
+    dtc.backend = 'RAW'
+    dtc.cell_name = 'vanilla'
+
+
+    for key, use_test in test_frame.items():
+        dtc.tests = use_test
+        dtc = dtc_to_rheo(dtc)
+        dtc = format_test(dtc)
+
+        if dtc.rheobase is not None:
+            if dtc.rheobase!=-1.0:
+
+                dtc = nunit_evaluation(dtc)
+
+        df[k][key] = int(dtc.get_ss())
+
+# A sparse grid sampling over the parameter space, using the published and well corrobarated parameter points, from Izhikitch publications, and the Open Source brain, shows that without optimization, using off the shelf parameter sets to fit real-life biological cell data, does not work so well.
+
+
+MU = 6
+NGEN = 150
+
+for key, use_test in test_frame.items():
+    ga_out, _ = om.run_ga(explore_param,NGEN,use_test,free_params=free_params, NSGA = True, MU = MU)
+
+    test_opt =  {str('multi_objective')+str(ga_out):ga_out}
+    with open('multi_objective.p','wb') as f:
+        pickle.dump(test_opt,f)
