fff74676d1a2927e888df101c4113b7e1a62f492
Rick Gerkin <rick1@gerkin.org>
2019-02-14 11:40:05 -0700

    Hotfix to pip install dash branch

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 import os
-
-try:
-    from pip.req import parse_requirements
-    from pip.download import PipSession
-except ImportError:
-    from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-
 from setuptools import setup, find_packages
 
+
 def read_requirements():
-    '''parses requirements from requirements.txt'''
+    """Parse requirements from requirements.txt."""
     reqs_path = os.path.join('.', 'requirements.txt')
-    install_reqs = parse_requirements(reqs_path, session=PipSession())
-    reqs = [str(ir.req) for ir in install_reqs]
-    return reqs
+    with open(reqs_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
 
 setup(
     name='neuronunit',
@@ -24,8 +18,9 @@ setup(
     packages=find_packages(),
     url='http://github.com/scidash/neuronunit',
     license='MIT',
-    description='A SciUnit library for data-driven testing of single-neuron physiology models.',
+    description=("A SciUnit library for data-driven testing of "
+                 "single-neuron physiology models."),
     long_description="",
-    test_suite="neuronunit.unit_test.core_tests",    
+    test_suite="neuronunit.unit_test.core_tests",
     install_requires=read_requirements(),
     )
