a57365fb1051754ecfeb386194b87dd3bcd1ff8a
Russell Jarvis <rjjarvis@asu.edu>
2019-03-07 13:11:32 -0700

    dev changes before merge
    Former-commit-id: e913fe91cdc8756fed1d83971e65790f9a411a30

--- light_install.sh
+++ light_install.sh
@@ -1,18 +1,21 @@
-
+#!/bin/bash
+sudo /home/russell/anaconda3/bin/conda install -y cython
+sudo /home/russell/anaconda3/bin/conda install -y numpy deap dask numba
 # move to a docker install script.
-sudo /opt/conda/bin/pip install git+https://github.com/scidash/sciunit.git@dev --process-dependency-links --upgrade
-sudo /opt/conda/bin/conda install -y cython
+sudo pip install git+https://github.com/scidash/sciunit.git@dev --upgrade #--process-dependency-links --upgrade
 
-# sudo /opt/conda/bin/pip install git+https://github.com/brian-team/brian2.git
+sudo pip3 install git+https://github.com/brian-team/brian2.git
 # faster glif
-#sudo /opt/conda/bin/pip install git+https://github.com/russelljjarvis/AllenSDK.git
-sudo /opt/conda/bin/conda remove -y bokeh
-sudo /opt/conda/bin/pip install pyNN lazyarray
+sudo pip3 install git+https://github.com/russelljjarvis/AllenSDK.git
+sudo pip3 install allensdk --upgrade
+sudo pip3 install -e . --ignore-installed
+
+
+#sudo /opt/conda/bin/conda remove -y bokeh
+#sudo /opt/conda/bin/pip install pyNN lazyarray
 #  PyGnuplot
 
 #sudo /opt/conda/bin/conda install -c benschneider pygnuplot
 
 #sudo apt-get install -y gnuplot
 
-sudo /opt/conda/bin/conda install -y numpy
-sudo /opt/conda/bin/pip install allensdk --upgrade
