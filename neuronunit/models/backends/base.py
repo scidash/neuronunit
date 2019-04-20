9ab73676ef638ed9201f14dd30f10f2a2c92b8fb
Justas Birgiolas <JustasB@users.noreply.github.com>
2019-04-20 16:26:26 -0700

    Update base.py

--- neuronunit/models/backends/base.py
+++ neuronunit/models/backends/base.py
@@ -20,26 +20,9 @@ from sciunit.models.backends import Backend, BackendException
 from sciunit.utils import dict_hash, import_module_from_path, \
                           TemporaryDirectory
 
-
-# Never import neuron in the current directory, or it will automatically
-# load mechanisms in that directory, which will then cause future calls
-# to load_mechanisms() to fail due to already loaded mechanisms.
-temp = TemporaryDirectory()
-curr = os.getcwd()
-os.chdir(temp.name)
-
-try:
-    import neuron
-    from neuron import h
-    NEURON_SUPPORT = True
-except:
-    neuron = None
-    h = None
-    NEURON_SUPPORT = False
-
-temp.cleanup()
-os.chdir(curr) 
-  
+# Test for NEURON support in a separate python process
+exit_code = os.system('python -c "import neuron"')
+NEURON_SUPPORT = (exit_code == 0)  
 
 try:
     import pyNN
