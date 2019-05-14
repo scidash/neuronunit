63a78c0530916f3d9965d8c7c59035e00dd7503d
Russell Jarvis <rjjarvis@asu.edu>
2019-05-13 19:36:44 -0700

    do stuff
    Former-commit-id: 50c2310860a8d3d146f483a69d906ec8855ab0c7

--- examples/simple_job.py
+++ examples/simple_job.py
@@ -47,9 +47,7 @@ from neuronunit.optimisation import get_neab
 import pickle
 from neuronunit import tests
 from neuronunit import neuroelectro
-import neuronunit.optimisation.model_parameters as model_params
-MODEL_PARAMS = model_params.MODEL_PARAMS
-MODEL_PARAMS['results'] = {}
+#MODEL_PARAMS['results'] = {}
 
 from neuronunit.optimisation import optimisation_management as om
 
@@ -88,6 +86,17 @@ def scale(X):
         X[:,i] = (temp-np.mean(temp))/(np.std(temp))
     return X, before
 
+def scale2(X, other=None):
+    before = copy.copy(X)
+    new = []
+    for i in range(0,np.shape(X)[1]):
+        temp = X[:,i]
+        #print(np.mean(temp)/np.std(temp))
+        ti = other[i]
+        new.append((ti-np.mean(temp))/(np.std(temp)))
+    print(new)
+    return new, before
+
 def un_scale(X,before):
     try:
         for i in range(0,np.shape(before)[1]):
@@ -113,20 +122,29 @@ from collections import Iterable, OrderedDict
 import quantities as qt
 rts,complete_map = pickle.load(open('../tests/russell_tests.p','rb'))
 local_tests = [value for value in rts['Hippocampus CA1 pyramidal cell'].values() ]
+#from neuronunit.optimisation.model_parameters import BAE1
+#params =
+import neuronunit.optimisation.model_parameters as model_params
+#MODEL_PARAMS = model_params.MODEL_PARAMS
+
+#params = model_paramaters.MODEL_PARAMS['BAE1']
 
 try:
-    #assert 1==2
+    assert 1==2
     ga_out = pickle.load(open('ca1.p','rb'))
 except:
-    ga_out, DO = om.run_ga(MODEL_PARAMS['RAW'], 100, local_tests, free_params = MODEL_PARAMS['RAW'],
-                                NSGA = True, MU = 10, model_type = str('RAW'))#,seed_pop=seeds[key])
+    ga_out, DO = om.run_ga(model_params.MODEL_PARAMS['BAE1'], 10, local_tests, free_params = model_params.MODEL_PARAMS['BAE1'],
+                                NSGA = True, MU = 10, model_type = str('ADEXP'))#,seed_pop=seeds[key])
     pickle.dump(ga_out,open('ca1.p','wb'))
+new_ind = copy.copy(ga_out['pf'][0])
 
 
 #X0_ = np.matrix([ list(ind.dtc.attrs.values()) for ind in ga_out['pf'] ])
 #Y = [ np.sum(ind.fitness.values) for ind in ga_out['pf']  ]
 
 Y = [ np.sum(v.fitness.values) for k,v in ga_out['history'].genealogy_history.items() ]
+Y_ = np.matrix([ list(v.fitness.values) for k,v in ga_out['history'].genealogy_history.items() ])
+
 X0_ = np.matrix([ list(v.dtc.attrs.values()) for k,v in ga_out['history'].genealogy_history.items() ])
 
 ordered_attrs = list(ga_out['history'].genealogy_history[1].dtc.attrs.keys())
@@ -136,28 +154,96 @@ new,b4 = scale(X0_)
 
 
 data0 = pd.DataFrame(X0_, columns = ordered_attrs)
-data1 = pd.DataFrame(X1, columns = ordered_attrs)
+#data1 = pd.DataFrame(X1, columns = ordered_attrs)
 le = preprocessing.LabelEncoder()
 le.fit(ordered_attrs)
 le.classes_
+new_ind.backend=str('ADEXP')
+new_ind.boundary_dict = model_params.MODEL_PARAMS['BAE1']
+
+from neuronunit.optimisation.optimisation_management import test_runner, cell_to_test_mapper
+from neuronunit.optimisation.optimisation_management import inject_and_plot
+#dtc = ga_out['pf'][0].dtc
+#inject_and_plot(dtc)
+
+import pdb; pdb.set_trace()
+_,dtc_pop = test_runner([new_ind],ordered_attrs,local_tests)
+print(sum(dtc_pop[0].scores.values()))
+#import pdb; pdb.set_trace()
+
+def generate_dataset(n_train, n_test, n_features, noise=0.1, verbose=False):
+    """Generate a regression dataset with the given parameters."""
+    if verbose:
+        print("generating dataset...")
+
+    #X, y, coef = make_regression(n_samples=n_train + n_test,
+    #                             n_features=n_features, noise=noise, coef=True)
+    #import pdb; pdb.set_trace()
+
+    random_seed = 13
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=n_train, test_size=n_test, random_state=random_seed)
+    X_train, y_train = shuffle(X_train, y_train, random_state=random_seed)
+
+    X_scaler = StandardScaler()
+    X_train = X_scaler.fit_transform(X_train)
+    X_test = X_scaler.transform(X_test)
+
+    y_scaler = StandardScaler()
+    y_train = y_scaler.fit_transform(y_train[:, None])[:, 0]
+    y_test = y_scaler.transform(y_test[:, None])[:, 0]
+
+    #gc.collect()
+    #if verbose:
+    #    print("ok")
+    return X_train, y_train, X_test, y_test
 
 
-X_train, X_test, Y_train, Y_test = train_test_split(new, Y, test_size = 0.3)
+X_scaler = StandardScaler()
+X0_ = X_scaler.fit_transform(X0_)
+X = X_scaler.transform(X0_)
+Y = X_scaler.fit_transform(Y_)
+Y = X_scaler.transform(Y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.3)
 sgd = SGDRegressor(penalty='l2', max_iter=1000, learning_rate='constant' , eta0=0.001  )
-sgd.fit(X_train, Y_train)
-hyperplane = sgd.coef_#+sgd.intercept_
+
+mlp = MLPRegressor(activation='relu', alpha=0.0001, batch_size='auto', beta_1=0.9,
+       beta_2=0.999, early_stopping=False, epsilon=1e-08,
+       hidden_layer_sizes=(13, 13, 13), learning_rate='constant',
+       learning_rate_init=0.001, max_iter=500, momentum=0.9,
+       nesterovs_momentum=True, power_t=0.5, random_state=None,
+       shuffle=True, solver='adam', tol=0.0001, validation_fraction=0.1,
+       verbose=False, warm_start=False)
+
+#sgd.fit(X_train, Y_train)
+mlp.fit(X_train, Y_train)
+
+hyperplane = mlp.coef_#+sgd.intercept_
+
 recovered = un_scale(hyperplane,b4)
+test_val = [ ga_out['pf'][0].dtc.attrs[k] for k in ordered_attrs ]
+X0_ = np.matrix([ list(v.dtc.attrs.values()) for k,v in ga_out['history'].genealogy_history.items() ])
+
+other,_ = scale2(X0_,other=test_val)
+pred = sgd.predict([other])
+#sgd.predict([other])
+
+
 gt  = ga_out['pf'][0].dtc.attrs
-new_ind = copy.copy(ga_out['pf'][0])
+
+
+
+for t in X_test:
+    print(ordered_attrs)
+    contents = un_scale(t,b4)#,ordered_attrs)
+    print([(contents[0,i],j) for i,j in enumerate(ordered_attrs)])
+    print(sgd.predict(t))
 
 for i, k in enumerate(ordered_attrs):
     print(gt[k],recovered[i],k)
     new_ind[i] = recovered[i]
     new_ind.dtc.attrs[k] = recovered[i]
-
-from neuronunit.optimisation.optimisation_management import test_runner
-_,dtc_pop = test_runner([new_ind],ordered_attrs,local_tests)
-print(sum(dtc_pop[0].scores.values()))
+#cell_to_test_mapper
 
 #pop,dtc_pop = test_runner([new_ind],ordered_attrs,local_tests)
 
