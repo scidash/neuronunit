9aad3d9a174fa7db0eaddfddf31ad1759605294b
Russell Jarvis <rjjarvis@asu.edu>
2019-05-28 11:23:51 +1000

    allen features

--- get_allen_features_from_nml_db.py
+++ get_allen_features_from_nml_db.py
@@ -32,11 +32,15 @@ from neuronunit.optimisation import get_neab
 from neuronunit.optimisation.optimisation_management import switch_logic#, active_values
 import efel
 from types import MethodType
+from neuronunit.optimisation.optimisation_management import init_dm_tests
 
 try:
-    with open('static_models.p','rb') as f:
-        sms = pickle.load(f)
+    assert 1==2
+    with open('static_models.p','rb') as f: sms = pickle.load(f)
+    with open('waves.p','rb') as f: waves = pickle.load(f)
 
+    #with open('waves.p','wb') as f:
+    #    pickle.dump(waves,f)
 except:
     def get_wave_forms(cell_id):
         url = str("https://www.neuroml-db.org/api/model?id=")+cell_id
@@ -50,56 +54,115 @@ except:
             url = str("https://neuroml-db.org/api/waveform?id=")+str(wid)
             waves = requests.get(url)
             temp = json.loads(waves.text)
-            if temp['Spikes'] == 1:
-                if 'NOISE' in temp['Protocol_ID']:
-                    print((temp['Waveform_Label']))
+            if 'NOISE' in temp['Protocol_ID']:
+                pass
+            if 'RAMP' in temp['Protocol_ID']:
+                pass
+            if 'SHORT_SQUARE_TRIPPLE' in temp['Protocol_ID']:
+                print((temp['Waveform_Label']))
+                pass
+            if str("1.0xRB") in temp['Waveform_Label'] or str("1.25xRB") in temp['Waveform_Label']:
+                print(temp['Protocol_ID'])
+                temp_vm = list(map(float, temp['Variable_Values'].split(',')))
+                waves_to_test['Times'] = list(map(float,temp['Times'].split(',')))
+                waves_to_test['DURATION'] = temp['Time_End'] -temp['Time_Start']
+                waves_to_test['DELAY'] = temp['Time_Start']
+                waves_to_test['Time_End'] = temp['Time_End']
+                waves_to_test['Time_Start'] = temp['Time_Start']
+                dt = waves_to_test['Times'][1]- waves_to_test['Times'][0]
+                waves_to_test['vm'] = AnalogSignal(temp_vm,sampling_period=dt*ms,units=mV)
 
+                #waves_to_test['everything'] = temp
+
+                sm = models.StaticModel(waves_to_test['vm'])
+                sm.complete = None
+                sm.complete = temp
+                sm.complete['vm'] = waves_to_test['vm']
+
+                #import pdb; pdb.set_trace()
+
+                trace = {}
+                DURATION = sm.complete['Time_End'] -sm.complete['Time_Start']
+
+                trace['T'] = waves_to_test['Times']
+                #trace['V'] = vm = [float(v[0]) for v in trace['V'].tolist()]
+
+                trace['V'] = temp_vm
+                #AnalogSignal(temp_vm,sampling_period=dt*ms,units=mV)
+                trace['stim_start'] = [sm.complete['Time_Start']]#rtest.run_params[]
+
+                trace['stim_end'] = [sm.complete['Time_End'] ]# list(sm.complete['duration'])
+                traces = [trace]# Now we pass 'traces' to the efel and ask it to calculate the feature# values
+                #import pdb; pdb.set_trace()
+                print(temp['Spikes'])
+                if temp['Spikes'] and np.min(temp_vm)<0:
+
+                    traces_results = efel.getFeatureValues(traces,list(efel.getFeatureNames()))#
+                    for v in traces_results:
+                        for key,value in v.items():
+                            if type(value) is not type(None):
+                                #pass
+                                print(key,value)
+                else:
+                    print(temp['Spikes'],np.min(temp_vm))
                     pass
-                if 'RAMP' in temp['Protocol_ID']:
-                    print((temp['Waveform_Label']))
 
-                    pass
-                if 'SHORT_SQUARE_TRIPPLE' in temp['Protocol_ID']:
-                    print((temp['Waveform_Label']))
-                    pass
 
-                if 'SHORT_SQUARE' in temp['Protocol_ID'] and not 'SHORT_SQUARE_TRIPPLE' in temp['Protocol_ID']:
-                    try:
-                        parts = temp['Waveform_Label'].split(' ')
-                        #import pdb; pdb.set_trace()
-                        print(parts[0])
-                        print(parts[1])
-                        waves_to_test['prediction'] = float(parts[0])*nA# '1.1133 nA',
-                        print('yes')
 
-                        temp_vm = list(map(float, temp['Variable_Values'].split(',')))
 
-                        waves_to_test['Times'] = list(map(float,temp['Times'].split(',')))
-                        waves_to_test['DURATION'] = temp['Time_End'] -temp['Time_Start']
-                        waves_to_test['DELAY'] = temp['Time_Start']
-                        waves_to_test['Time_End'] = temp['Time_End']
-                        waves_to_test['Time_Start'] = temp['Time_Start']
-                        dt = waves_to_test['Times'][1]- waves_to_test['Times'][0]
-                        waves_to_test['vm'] = AnalogSignal(temp_vm,sampling_period=dt*ms,units=mV)
-                        waves_to_test['everything'] = temp
-                        waves_to_get.append(waves_to_test)
+            if 'SHORT_SQUARE' in temp['Protocol_ID'] and not 'SHORT_SQUARE_TRIPPLE' in temp['Protocol_ID'] or 'SHORT_SQUARE_TRIPPLE' in temp['Protocol_ID']:
+                try:
+                    parts = temp['Waveform_Label'].split(' ')
 
-                    except:
-                        if temp['Waveform_Label'] is None:
-                            pass
+                    #import pdb; pdb.set_trace()
+                    print(' value ',parts[0], ' units ',parts[1])
+
+                    waves_to_test['prediction'] = float(parts[0])*nA# '1.1133 nA',
+                    #print('yes')
+
+                    temp_vm = list(map(float, temp['Variable_Values'].split(',')))
+
+                    waves_to_test['Times'] = list(map(float,temp['Times'].split(',')))
+                    waves_to_test['DURATION'] = temp['Time_End'] -temp['Time_Start']
+                    waves_to_test['DELAY'] = temp['Time_Start']
+                    waves_to_test['Time_End'] = temp['Time_End']
+                    waves_to_test['Time_Start'] = temp['Time_Start']
+                    dt = waves_to_test['Times'][1]- waves_to_test['Times'][0]
+                    waves_to_test['vm'] = AnalogSignal(temp_vm,sampling_period=dt*ms,units=mV)
+                    waves_to_test['everything'] = temp
+                    waves_to_get.append(waves_to_test)
+
+                except:
+                    if temp['Waveform_Label'] is None:
                         pass
+                    pass
         return waves_to_get
     waves = get_wave_forms(str('NMLCL001129'))
-    sms = []
-    for w in waves:
+    with open('waves.p','wb') as f:
+        pickle.dump(waves,f)
 
-        sm = models.StaticModel(w['vm'])
-        sm.rheobase = {}
-        sm.rheobase['mean'] = w['prediction']
-        sm.complete = w
-        sms.append(sm)
+
+    #import pdb
+    #pdb.set_trace()
     with open('static_models.p','wb') as f:
         pickle.dump(sms,f)
+
+
+protocols = [ w['everything']['Protocol_ID'] for w in waves ]
+ss = [ w['everything'] for w in waves if w['everything']['Protocol_ID'] == 'SHORT_SQUARE' ]
+#not_current_injections = [ w for w in waves if w['everything']['Spikes'] > 1 ]
+
+not_current_injections = [ w for w in waves if w['everything']['Variable_Name'] != str('Current') ]
+sms = []
+for w in not_current_injections:
+
+    sm = models.StaticModel(w['vm'])
+    sm.rheobase = {}
+    sm.rheobase['mean'] = w['prediction']
+    sm.complete = w
+    sms.append(sm)
+
+current_injections = [ w for w in waves if w['everything']['Variable_Name'] == str('Current') ]
 
 electro_path = str(os.getcwd())+'/examples/pipe_tests.p'
 
@@ -119,24 +182,23 @@ def generate_prediction(self,model):
 test_scores = []
 tt = [tests for tests in test_frame[0][0] ]
 #vtest[k] = active_values(keyed,dtc.rheobase)
-#for t in tt: t.generate_prediction = MethodType(generate_prediction,t)
+for t in tt: t.generate_prediction = MethodType(generate_prediction,t)
 params = {}
 
-
+'''
 def get_m_p(cls,params = {}):
     return model.get_membrane_potential()
 for model in sms:
     model.inject_square_current = MethodType(get_m_p,model)#get_membrane_potential
-
+'''
 #def get_membrane_potential():
 
 #for model in sms: model.inject_square_current = MethodType(model.get_membrane_potential,model)
 
 #for model in sms: model.inject_square_current = MethodType(model.get_membrane_potential,model)
 #for model in sms: print(model.inject_square_current())# = MethodType(model.get_membrane_potential,params)
+#for t in tt: t.generate_prediction = MethodType(generate_prediction,t)
 
-tt[0].generate_prediction = MethodType(generate_prediction,tt[0])
-#print(active_values.__file__)
 def active_values(keyed,rheobase,square = None):
     keyed['injected_square_current'] = {}
     if square == None:
@@ -166,7 +228,11 @@ for t in tt:
         t.params = active_values(t.params,rheobase,square=sm.complete)
 
 
-flat_iter = iter((t,sm) for t in tt for sm in sms)
+flat_iter = [(t,sm) for t in tt for sm in sms]
+try:
+    from neuronunit.optimisation.optimisation_management import add_druckmann_properties_to_cells as dm
+except:
+    from neuronunit.optimisation.optimisation_management import add_dm_properties_to_cells as dm
 
 for t,sm in flat_iter:
     sm._backend = None
@@ -176,12 +242,18 @@ for t,sm in flat_iter:
 
         #score = rtest.judge(model)
         results = sm.get_membrane_potential()
+        value = float(np.max(current_injections[0]['vm']))
+
+        dm_tests = init_dm_tests(value,1.5*value)
+
+        predictions = [ dm.generate_prediction(sm) for dm in dm_tests ]
+        #import pdb; pdb.set_trace()
         trace = {}
+        DURATION = sm.complete['Time_End'] -sm.complete['Time_Start']
 
         trace['T'] = sm.complete['Times']
         trace['V'] = results
         trace['stim_start'] = [sm.complete['Time_Start']]#rtest.run_params[]
-        DURATION = sm.complete['Time_End'] -sm.complete['Time_Start']
 
         trace['stim_end'] = [sm.complete['Time_End'] ]# list(sm.complete['duration'])
         traces = [trace]# Now we pass 'traces' to the efel and ask it to calculate the feature# values
@@ -189,18 +261,21 @@ for t,sm in flat_iter:
         for v in traces_results:
             for key,value in v.items():
                 if type(value) is not type(None):
+                    #pass
                     print(key,value)
-        try:
-            test_scores.append(t.judge(sm))
-            print(test_scores[-1],t.name)
-        except:
-            test_scores.append(t.judge(sm))
+        #try:
+            #import pdb; pdb.set_trace()
+        #    sm.get_membrane_potential()
+        #    test_scores.append(t.judge(sm))
+        #    print(test_scores[-1],t.name)
+        #except:
+            #test_scores.append(t.judge(sm))
 
-            print(test_scores[-1],t.name)
+            #print(test_scores[-1],t.name)
 
             print('active skipped: ',t.name)
-
-
+        print(test_scores)
+import pdb; pdb.set_trace()
         #all_models = json.loads(url.read().decode())
 def get_all(Model_ID = str('NMLNT001592')):
     if Model_ID == None:
