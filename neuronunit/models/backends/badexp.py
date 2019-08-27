bug fix in backends units

neuronunit/models/backends/badexp.py
@@ -87,7 +87,7 @@ class ADEXPBackend(Backend):
         """Must return a neo.core.AnalogSignal.
         And must destroy the hoc vectors that comprise it.
         """
-        if np.max(self.vM)!=0.020*qt.V:
+        if np.max(self.vM)<0.000*qt.V:
             tdic = self.spike_monitor.spike_trains()
             for key,value in tdic.items():
                 if len(value)==1:
@@ -157,9 +157,9 @@ class ADEXPBackend(Backend):
             c = current['injected_square_current'];
         else:
             c = current
-        amplitude = float(c['amplitude'])#*1000.0
-        duration = int(c['duration'])#/dt#/dt.rescale('ms')
-        delay = int(c['delay'])#/dt#.rescale('ms')
+        amplitude = float(c['amplitude'].simplified)#*1000.0
+        duration = int(c['duration'].simplified)#/dt#/dt.rescale('ms')
+        delay = int(c['delay'].simplified)#/dt#.rescale('ms')
         pre_current = int(duration)+100
         stim = input_factory.get_step_current(int(delay), int(pre_current), 1 * b2.ms, amplitude *b2.pA)
         st = (duration+delay+100)* b2.ms

neuronunit/models/backends/rawpy.py
@@ -149,12 +149,14 @@ class RAWBackend(Backend):
             c = current['injected_square_current']
         else:
             c = current
-        amplitude = float(c['amplitude'])
+        #import pdb; pdb.set_trace()
+        amplitude = float(c['amplitude'].simplified) #this needs to be in every backends
+        #print(amplitude)
 
         duration = float(c['duration'])#/dt#/dt.rescale('ms')
         delay = float(c['delay'])#/dt#.rescale('ms')
         if 'sim_length' in c.keys():
-            sim_length = c['sim_length']
+            sim_length = c['sim_length']#.simplified
         tMax = delay + duration + 200.0#/dt#*pq.ms
         self.set_stop_time(tMax*pq.ms)
         tMax = self.tstop
@@ -172,7 +174,11 @@ class RAWBackend(Backend):
         Iext[delay_ind+duration_ind::] = 0.0
 
         attrs['Iext'] = Iext
+        #print(attrs)
+        #if len(attrs)>1:
         v = get_vm(**attrs)
+        #else:
+        #    v = get_vm(attrs)
         #print(np.max(v))
 
         self.vM = AnalogSignal(v,