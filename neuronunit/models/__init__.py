fef2a4b36b1c52506852cddd784e815d454a8735
Russell Jarvis <rjjarvis@asu.edu>
2019-09-26 16:09:49 +1000

    build for travis reconfigure and opt out of text plots

--- neuronunit/models/__init__.py
+++ neuronunit/models/__init__.py
@@ -4,6 +4,6 @@ import warnings
 from .static import StaticModel, ExternalModel
 from .lems import LEMSModel
 from .channel import ChannelModel
-from .reduced import ReducedModel
+from .reduced import ReducedModel, VeryReducedModel
 from . import backends  # Required to register backends
-from .very_reduced import VeryReducedModel
+#from .very_reduced import 
