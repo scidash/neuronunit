755883f2f8c12b29858d4365c8cc3b0f733ee6c4
Russell Jarvis <colouredstatic@gmail.com>
2019-11-21 09:18:28 +1100

    Update use_edt.py
    Former-commit-id: 0c4f826448c8334aa5dc2365be7d6459961a43d2

--- neuronunit/examples/use_edt.py
+++ neuronunit/examples/use_edt.py
@@ -2,4 +2,4 @@ import elephant_data_tests
 a = elephant_data_tests.testHighLevelOptimisation()
 a.setUp()
 out = a.test_data_driven_ae()
-out = a.test_data_driven_rt_ae()
+out = a.test_not_data_driven_rt_ae()
