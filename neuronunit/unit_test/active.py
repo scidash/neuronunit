af86c8c31d0fd35daac6f929cdba50aab4ba2d0c
Rick Gerkin <rgerkin@asu.edu>
2020-01-23 22:46:19 +0000

    Moved most unit tests import outside of __init__ to allow 'unittest -m syntax' without running every test
    Former-commit-id: fadf87bb187da2c98849a1daf8f1597c08b46f5c

---
+++ neuronunit/unit_test/active.py
@@ -0,0 +1,20 @@
+from .base import *
+from .import_tests import ImportTestCase
+from .test_high_level import testHighLevelOptimisation
+from .test_low_level import testLowLevelOptimisation
+from .doc_tests import DocumentationTestCase
+from .resource_tests import NeuroElectroTestCase, BlueBrainTestCase,\
+                            AIBSTestCase
+from .model_tests import ReducedModelTestCase, ExtraCapabilitiesTestCase
+from .observation_tests import ObservationsTestCase
+from .test_tests import TestsPassiveTestCase, TestsWaveformTestCase,\
+                        TestsFITestCase, TestsDynamicsTestCase,\
+                        TestsChannelTestCase
+from .misc_tests import EphysPropertiesTestCase
+from .sciunit_tests import SciUnitTestCase
+from .cache_tests import BackendCacheTestCase
+
+from .test_druckmann2013 import Model1TestCase, Model2TestCase, \
+    Model3TestCase, Model4TestCase, Model5TestCase, \
+    Model6TestCase, Model7TestCase, Model8TestCase, Model9TestCase, \
+    Model10TestCase, Model11TestCase
