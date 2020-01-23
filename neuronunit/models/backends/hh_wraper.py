50a1d67f282113f025b9b02f1e91106d580d4a70
Russell Jarvis <colouredstatic@gmail.com>
2020-01-23 18:01:38 +1100

    push back to master
    Former-commit-id: ba75e6b22e9a7305ea9865e3b878a42cdbbd5855

--- neuronunit/models/backends/hh_wraper.py
+++ neuronunit/models/backends/hh_wraper.py
@@ -6,7 +6,7 @@ jl.eval("using SpikingNeuralNetworks")
 jl.eval("SNN = SpikingNeuralNetworks")
 jl.eval('include("units.jl")')
 jl.eval('include("plot.jl")')
-Main.eval("using Debugger")
+# Main.eval("using Debugger")
  
 import io
 import math
