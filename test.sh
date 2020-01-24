31a1008f4c1530d3eeb3f52c2145161660f5892a
Russell Jarvis <rjjarvis@asu.edu>
2020-01-24 12:12:32 +1100

    before merge to sqaush
    Former-commit-id: ae6dd349403eeb34a5a3fcca34250ee264ec80f0

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #UNIT_TEST_SUITE="neuronunit.unit_test buffer"
 # Fundamental Python bug prevents this other method from allowing
-# some notebook tests to pass.  
+# some notebook tests to pass.
 #UNIT_TEST_SUITE="setup.py test"
 #coverage run -m --source=. --omit=*unit_test*,setup.py,.eggs $UNIT_TEST_SUITE
 coverage run neuronunit/unit_test/low_level_test.py
@@ -17,17 +17,16 @@ done
 if [ $run_all ]; then
   coverage run neuronunit/examples/use_edt.py
 fi
-
-cd neuronunit/examples
-jupyter nbconvert --to notebook --execute chapter1.ipynb
-jupyter nbconvert --to notebook --execute chapter10.ipynb
-jupyter nbconvert --to notebook --execute chapter11.ipynb
-jupyter nbconvert --to notebook --execute chapter2_needs_merge.ipynb
-jupyter nbconvert --to notebook --execute chapter3.ipynb
-jupyter nbconvert --to notebook --execute chapter4.ipynb
-jupyter nbconvert --to notebook --execute chapter5.ipynb
-jupyter nbconvert --to notebook --execute chapter6.ipynb
-jupyter nbconvert --to notebook --execute chapter7.ipynb
-jupyter nbconvert --to notebook --execute chapter8.ipynb
-jupyter nbconvert --to notebook --execute chapter9.ipynb
-
+python neuronunit/unit_test/doc_tests.py
+#cd neuronunit/examples
+#jupyter nbconvert --to notebook --execute chapter1.ipynb
+#jupyter nbconvert --to notebook --execute chapter10.ipynb
+#jupyter nbconvert --to notebook --execute chapter11.ipynb
+#jupyter nbconvert --to notebook --execute chapter2_needs_merge.ipynb
+#jupyter nbconvert --to notebook --execute chapter3.ipynb
+#jupyter nbconvert --to notebook --execute chapter4.ipynb
+#jupyter nbconvert --to notebook --execute chapter5.ipynb
+#jupyter nbconvert --to notebook --execute chapter6.ipynb
+#jupyter nbconvert --to notebook --execute chapter7.ipynb
+#jupyter nbconvert --to notebook --execute chapter8.ipynb
+#jupyter nbconvert --to notebook --execute chapter9.ipynb
