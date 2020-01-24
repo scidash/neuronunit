ae6dd349403eeb34a5a3fcca34250ee264ec80f0
Russell Jarvis <rjjarvis@asu.edu>
2020-01-24 12:12:32 +1100

    before merge to sqaush

--- neuronunit/unit_test/doc_tests.py
+++ neuronunit/unit_test/doc_tests.py
@@ -6,6 +6,37 @@ class DocumentationTestCase(NotebookTools,
                             unittest.TestCase):
     """Testing documentation notebooks"""
 
+    path = '../examples'
+
+
+    def test_elaborate(self):
+        self.do_notebook('../examples/elaborate_backend_tests')
+    def test_bbp(self):
+        self.do_notebook('../unit_tests/bbp')
+
+    def test_chapter1_opt(self):
+        self.do_notebook('../examples/chapter1')
+    def test_chapter2_opt(self):
+        self.do_notebook('../examples/chapter2_needs_merge')
+    def test_chapter3_opt(self):
+        self.do_notebook('../examples/chapter3')
+    def test_chapter4_opt(self):
+        self.do_notebook('../examples/chapter4')
+    def test_chapter5_opt(self):
+        self.do_notebook('../examples/chapter5')
+    def test_chapter6_opt(self):
+        self.do_notebook('../examples/chapter6')
+    def test_chapter7_opt(self):
+        self.do_notebook('../examples/chapter7')
+    def test_chapter8_opt(self):
+        self.do_notebook('../examples/chapter8')
+    def test_chapter9_opt(self):
+        self.do_notebook('../examples/chapter9')
+    def test_chapter10_opt(self):
+        self.do_notebook('../examples/chapter10')
+    def test_chapter11_opt(self):
+        self.do_notebook('../examples/chapter11')
+
     path = '../../docs'
 
     #@unittest.skip("Skipping chapter 1")
