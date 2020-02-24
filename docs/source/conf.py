2ed887817046361c249be8a18f364f6135a12832
Richard C Gerkin <rgerkin@asu.edu>
2020-02-24 15:02:58 -0700

    Remove autodoc from neuronunit to avoid timeout

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,7 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
+extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages']
 
