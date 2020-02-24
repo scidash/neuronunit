2e71ae2fce1b2a7334dd3d82b5bb24fef8800a43
Richard C Gerkin <rgerkin@asu.edu>
2020-02-24 16:36:21 -0700

    Set root docs page to index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,9 @@ extensions = ['sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages']
 
+# The master toctree document.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
