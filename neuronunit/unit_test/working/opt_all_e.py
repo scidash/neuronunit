b73844f5c0edd0b86d00002020893fe017836c2e
Russell Jarvis <rjjarvis@asu.edu>
2020-03-02 17:05:42 +1100

    files dones
    Former-commit-id: 693a0e331aedf5a9576ce0187e2a2be7e09d3da3

--- neuronunit/unit_test/working/opt_all_e.py
+++ neuronunit/unit_test/working/opt_all_e.py
@@ -1,79 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-#get_ipython().run_line_magic('matplotlib', 'inline')
-import seaborn as sns
-#sns.set_context('notebook')
-#get_ipython().run_line_magic('config', "InlineBackend.figure_format = 'retina'")
-#%inline
-#import matplotlib as mpl
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-#%matplotlib inline
+import hide_imports
+import copy
 plt.plot([0,1],[1,0])
 plt.show()
 
-
-# In[2]:
-
-
-import hide_imports
-
-
-# In[3]:
-
-
-
 plt.clf()
-import copy
-get_ipython().run_line_magic('matplotlib', 'inline')
 
-def permutations(use_test,backend):
+def permutations(use_test,backend,MU=100,NGEN=100):
     use_test = hide_imports.TSD(use_test)
     use_test.use_rheobase_score = True
     edges = hide_imports.model_parameters.MODEL_PARAMS[backend]
-    ga_out0 = use_test.optimize(edges,backend=backend,        protocol={'allen': False, 'elephant': True}, MU=8,NGEN=1)
-    ga_out1 =  use_test.optimize(edges,backend=backend,        protocol={'allen': False, 'elephant': True},            MU=8,NGEN=8,seed_pop=ga_out0['pf'][0])
-
-    
-    dtc = ga_out1['pf'][0].dtc
-    vm,plt = hide_imports.inject_and_plot_model(dtc.attrs,dtc.backend)
-    plt.show()
+    ga_out = use_test.optimize(edges,backend=backend,protocol={'allen': False, 'elephant': True}, MU=MU,NGEN=NGEN)
+    dtc = ga_out['pf'][0].dtc
+    vm,plt = hide_imports.inject_and_plot_model(dtc)
     return dtc, ga_out1['DO'], vm
 
 
-# In[4]:
-
-
-test_frame = hide_imports.get_neab.process_all_cells()
-test_frame.pop('Olfactory bulb (main) mitral cell',None)
+#test_frame.pop('Olfactory bulb (main) mitral cell',None)
 OMObjects = []
 backends = ["RAW","HH"]#"ADEXP","BHH"]
 t = test_frame['Neocortex pyramidal cell layer 5-6']
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 backends = ["RAW","HH","ADEXP","BHH"]
@@ -81,16 +31,9 @@ backends = ["RAW","HH","ADEXP","BHH"]
 
 for t in test_frame.values():
     b = backends[0]
-    (dtc,DO,vm) = permutations(copy.copy(t),b)
+    (dtc,DO,vm) = permutations(copy.copy(t),b,MU,NGEN)
     display(dtc.SM)
     display(dtc.obs_preds)
-    #plt.plot(vm.times,vm.magnitude)
-    #plt.show()
-
-
-
-
-# In[ ]:
 
 
 for t in test_frame.values():
