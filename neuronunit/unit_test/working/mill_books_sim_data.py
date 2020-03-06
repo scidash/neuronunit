082c6edd4111aa93b751745bcacef158ba26514c
Russell Jarvis <rjjarvis@asu.edu>
2020-03-06 12:48:45 +1100

    repaired simulated data part
    Former-commit-id: 88476985939b30837c9316de1a17ad870ac65bfc

--- neuronunit/unit_test/working/mill_books_sim_data.py
+++ neuronunit/unit_test/working/mill_books_sim_data.py
@@ -1,12 +1,13 @@
 import papermill as pm
 import sklearn
-backends = ["RAW", "HH"]
+backends = [str("RAW"), str("HH")]
 
 #samples = [75,100]
 samples_sd = [75,100,125,150]
-for s in samples_sd:
-    pm.execute_notebook(
-        'simulated_data_paramaterized.ipynb',
-        'milled_nbooks_{0}_{1}.ipynb'.format(s,s),
-        parameters = dict(backends=backends,NGEN =s, MU =s)
-    )
+for b in backends:
+    for s in samples_sd:
+        pm.execute_notebook(
+            'simulated_data_paramaterized_2.ipynb',
+            'milled_nbooks_{0}_{1}.ipynb'.format(s,s),
+            parameters = dict(backend=b,NGEN =s, MU =s)
+        )
