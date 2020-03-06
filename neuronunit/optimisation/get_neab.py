c4748fc01a44d8b928f9435cb1d3f555598977f7
Russell Jarvis <colouredstatic@gmail.com>
2020-03-06 16:45:54 +1100

    before placement on HPC
    Former-commit-id: 688b2cc9a514a04c2f422e040042bcedb42aa1ae

--- neuronunit/optimisation/get_neab.py
+++ neuronunit/optimisation/get_neab.py
@@ -193,7 +193,7 @@ def get_neuron_criteria(cell_id,file_name = None):#,observation = None):
     observations = {}
     for index, t in enumerate(test_classes):
         obs = t.neuroelectro_summary_observation(cell_id)
-        
+
         if obs is not None:
             if 'mean' in obs.keys():
                 print(test_classes[index])
@@ -275,13 +275,11 @@ def switch_logic(xtests):
 
 def process_all_cells():
     try:
-        assert 1==2
         with open('processed_multicellular_constraints.p','rb') as f:
             filtered_cells = pickle.load(f)
         return filtered_cells
     except:
         try:
-            assert 1==2
             cell_constraints = pickle.load(open("multicellular_suite_constraints.p","rb"))
         except:
 	        cell_constraints = get_all_cells()
@@ -313,7 +311,7 @@ def process_all_cells():
             assert hasattr(t,'passive')
         for t in filtered_cells[key].tests:
             assert hasattr(t,'active')
-            assert hasattr(t,'passive')	
+            assert hasattr(t,'passive')
         """
         with open('processed_multicellular_constraints.p','wb') as f:
            pickle.dump(filtered_cells,f)
