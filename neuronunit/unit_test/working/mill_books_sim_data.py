74c21dd30c0fc615569510ce3f94429e0462b213
Russell Jarvis <rjjarvis@asu.edu>
2020-04-07 12:35:22 +1000

    notebooks debugged

--- neuronunit/unit_test/working/mill_books_sim_data.py
+++ neuronunit/unit_test/working/mill_books_sim_data.py
@@ -3,18 +3,14 @@ warnings.filterwarnings('ignore')
 s = io.StringIO()
 
 import papermill as pm
-#import sklearn
-#backends = [str("RAW"), str("HH")]
-backends = str("RAW")#, str("HH")]
+backends = [str("RAW"), str("HH")]
 
-#samples = [75,100]
-samples_sd = [5,10,12]
-#for b in backends:
-b = str("RAW")
+pop_size = [20,40,60,80,100]
 with contextlib.redirect_stdout(s):
-   for s in samples_sd:
-       pm.execute_notebook(
-       'simulated_data_paramaterized_3.ipynb',
-       'milled_nbooks_{0}_{1}.ipynb'.format(s,s),
-       parameters = dict(backend=b,NGEN =10, MU =s)
-    )
+   for b in backends:
+      for s in pop_size:
+         pm.execute_notebook(
+         'simulated_data_paramaterized_3.ipynb',
+         'milled_nbooks_{0}_{1}_{2}.ipynb'.format(s,s,b),
+         parameters = dict(backend=b,NGEN =100, MU =s)
+      )
