54050f13778557e00117f3c8f4a89dc7f938a67f
Russell Jarvis <rjjarvis@asu.edu>
2020-04-07 19:23:17 -0700

    sync HPC changes for local Machine
    Former-commit-id: 1c7d54deac80e3f4729b73e87ca373902d5abc48

--- neuronunit/unit_test/working/mill_books_data_driven.py
+++ neuronunit/unit_test/working/mill_books_data_driven.py
@@ -12,14 +12,13 @@ test_types = ['Neocortex pyramidal cell layer 5-6', \
     ]
 models = [str("RAW"), str("HH")]
 
-samples_sd = [10,20,30,100]
-
+#samples_sd = [10,20,30,100]
+MU = 20 
 with contextlib.redirect_stdout(s):
     for test_type in test_types:
         for model in models:
-            for MU in samples_sd:
-                pm.execute_notebook(
+            pm.execute_notebook(
                     'paramaterized.ipynb',
-                    'milled_nbooks_{0}_{1}.ipynb'.format(model,test_type),
+                    'milled_nbooks_{0}_{1}_{2}.ipynb'.format(model,test_type,MU),
                     parameters = dict(model_type=str(model), test_type=test_type, NGEN =105, MU =MU)
             )
--- neuronunit/unit_test/working/mill_books_sim_data.py
+++ neuronunit/unit_test/working/mill_books_sim_data.py
@@ -5,12 +5,13 @@ s = io.StringIO()
 import papermill as pm
 backends = [str("RAW"), str("HH")]
 
-pop_size = [20,40,60,80,100]
+#pop_size = [20,40,60,80,100]
+s = 40
 with contextlib.redirect_stdout(s):
    for b in backends:
-      for s in pop_size:
-         pm.execute_notebook(
+      #for s in pop_size:
+      pm.execute_notebook(
          'simulated_data_paramaterized_3.ipynb',
-         'milled_nbooks_{0}_{1}_{2}.ipynb'.format(s,s,b),
-         parameters = dict(backend=b,NGEN =100, MU =s)
+         'milled_nbooks_sim_data_{0}_{1}_{2}.ipynb'.format(s,s,b),
+         parameters = dict(backend=b,NGEN =200, MU =s)
       )
