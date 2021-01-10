e3d7b0046bdf14fa0299b6e64038fc443a2890d8
Russell Jarvis <rjjarvis@asu.edu>
2021-01-10 14:12:35 +1100

    adding back in important looking tests

---
+++ neuronunit/unit_test/base.py
@@ -0,0 +1,22 @@
+"""Common imports for many unit tests in this directory"""
+
+import unittest
+import sys
+import os
+import warnings
+import pdb
+from urllib.request import urlretrieve
+
+import matplotlib as mpl
+
+OSX = sys.platform == 'darwin'
+if OSX or 'Qt' in mpl.rcParams['backend']:
+    mpl.use('Agg')  # Avoid any problems with Macs or headless displays.
+
+from sciunit.utils import NotebookTools, import_all_modules
+import neuronunit
+from neuronunit.models import ReducedModel
+from neuronunit import neuroelectro, bbp, aibs, tests as nu_tests
+
+NU_BACKEND = os.environ.get('NU_BACKEND', 'jNeuroML')
+NU_HOME = neuronunit.__path__[0]
