784b26def1374f1432338f18afed20572d44ce5b
Russell Jarvis <russelljarvis@protonmail.com>
2021-01-25 16:05:45 +1100

    update unit test for refactor rheobase_dtc_tests

--- neuronunit/unit_test/rheobase_dtc_test.py
+++ neuronunit/unit_test/rheobase_dtc_test.py
@@ -1,32 +1,15 @@
 import unittest
-
 #!/usr/bin/env python
 # coding: utf-8
 import matplotlib
 
-matplotlib.use("Agg")
-# from bluepyopt.allenapi.allen_data_driven import opt_setup, opt_setup_two, opt_exec, opt_to_model
-from neuronunit.optimization.optimization_management import check_bin_vm15
-from neuronunit.optimization.model_parameters import (
-    MODEL_PARAMS,
-    BPO_PARAMS,
-    to_bpo_param,
-)
-from neuronunit.optimization.optimization_management import (
-    dtc_to_rheo,
-    inject_and_plot_model,
-)
-
-# from bluepyopt.allenapi.allen_data_driven import opt_to_model
-# from bluepyopt.allenapi.utils import dask_map_function
 import numpy as np
+from neuronunit.optimization.model_parameters import MODEL_PARAMS, BPO_PARAMS, to_bpo_param
+from neuronunit.optimization.optimization_management import dtc_to_rheo,inject_and_plot_model
 from neuronunit.optimization.data_transport_container import DataTC
-
-# import efel
 from jithub.models import model_classes
 import matplotlib.pyplot as plt
 import quantities as qt
-
 
 class testOptimization(unittest.TestCase):
     def setUp(self):
@@ -46,7 +29,7 @@ class testOptimization(unittest.TestCase):
         dtc.backend = cellmodel
         dtc._backend = model._backend
         dtc.attrs = model.attrs
-        dtc.params = {k: np.mean(v) for k, v in MODEL_PARAMS[cellmodel].items()}
+        dtc.params = {k:np.mean(v) for k,v in MODEL_PARAMS[cellmodel].items()}
         other_params = BPO_PARAMS[cellmodel]
         dtc = dtc_to_rheo(dtc)
         assert dtc.rheobase is not None
